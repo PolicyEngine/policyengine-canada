25a98c56c37974670c954b40c63b303942bc8273
Nikhil Woodruff <nikhil@policyengine.org>
2022-10-18 17:03:18 +0100

    Set initial versioning

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ dev_requirements = [
 
 setup(
     name="policyengine-canada",
-    version="0.1.0",
+    version="0.0.0",
     author="PolicyEngine",
     author_email="hello@policyengine.org",
     classifiers=[
