ee0273a124b41a6f01a6efc896fa5d1395a944cf
Alex Rand <alexrand94@gmail.com>
2022-10-24 19:28:38 -0400

    Adding 'total pre-tax income' for individuals and households

--- policyengine_canada/variables/household/income/individual/self_employment_income.py
+++ policyengine_canada/variables/household/income/individual/self_employment_income.py
@@ -9,4 +9,3 @@ class self_employment_income(Variable):
     documentation = "Income from gainful employment"
     definition_period = YEAR
     reference = "https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/personal-income/self-employment-income-lines-13499-14299-gross-income-lines-13500-14300-net-income.html"
-
