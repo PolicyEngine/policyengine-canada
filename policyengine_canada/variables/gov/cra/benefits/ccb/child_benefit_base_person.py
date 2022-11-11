a477c76f1ec5733b207c2d00b59dab5ec775e077
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-11-11 13:43:54 -0500

    need more tests

--- policyengine_canada/variables/gov/cra/benefits/ccb/child_benefit_base_person.py
+++ policyengine_canada/variables/gov/cra/benefits/ccb/child_benefit_base_person.py
@@ -3,14 +3,12 @@ from policyengine_canada.model_api import *
 
 class child_benefit_base_person(Variable):
     value_type = float
-    entity = Household
+    entity = Person
     label = "Canada Child Benefit Base"
     unit = CAD
     documentation = "Base amount of Canada Child Benefit before reduction."
     definition_period = YEAR
 
-    def formula(household, period, parameters):
-        person = household.members
+    def formula(person, period, parameters):
         age = person("age", period)
-        person_amount = parameters(period).gov.cra.benefits.ccb.base.calc(age)
-        return household.sum(person_amount)
+        return parameters(period).gov.cra.benefits.ccb.base.calc(age)
