4c961d49828d6ce761483f07fed7303e9cc3b0e7
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-11-13 10:50:07 -0500

    folder structure

---
+++ policyengine_canada/variables/gov/cra/tax/income/credits/climate_action/climate_action_incentive.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class climate_action_incentive(Variable):
+    value_type = float
+    entity = Person
+    label = "Canada Climate Action Incentive"
+    unit = CAD
+    documentation = "Universal amount without adjustment based on AFNI"
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        gov = parameters(
+            period
+        ).gov.cra.tax.income.credits.climate_action.amount
+
+
+# TODO: state codes for Canada + select statements based on the states
