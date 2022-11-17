2146fc5f90944b9e75800e4070e218aab3487b42
Alex Rand <alexrand94@gmail.com>
2022-11-17 12:37:17 -0500

    debugging eligibility test

--- policyengine_canada/variables/gov/cra/benefit/old_age_security_pension/old_age_security_pension_pre_clawback.py
+++ policyengine_canada/variables/gov/cra/benefit/old_age_security_pension/old_age_security_pension_pre_clawback.py
@@ -10,11 +10,13 @@ class old_age_security_pension_pre_clawback(Variable):
     unit = CAD
     definition_period = YEAR
 
-    def formula(tax_unit, period, parameters):
-        eligible = tax_unit("old_age_security_pension_eligibility", period)
+    def formula(person, period, parameters):
+        age = person("age", period)
+        adult_years_in_canada = person("adult_years_in_canada", period)
+        eligible = person("old_age_security_pension_eligibility", period)
         oas_pension = parameters(
             period
-        ).gov.cra.benefits.old_age_security_pension  # shortcut
+        ).gov.cra.tax.income.benefits.old_age_security_pension  # shortcut
         older_increase_age_threshold = (
             oas_pension.age_eligibility_older_seniors_increase
         )  # age at which you get the percentage boost
@@ -31,7 +33,7 @@ class old_age_security_pension_pre_clawback(Variable):
         )  # Your full base amount is your number of adult residence years divided by the number of years at which you are eligible for 100%. In the SPSD/M 29.0 this is imoasres
         return (
             eligible
-            * maximum_amount
+            * base_amount
             * scale_factor
-            * total_older_increase_factors
+            * total_older_increase_factor
         )  # apply the old age boost to the base amount if applicable. In the SPSD/M 29.0 this is imoasmax
