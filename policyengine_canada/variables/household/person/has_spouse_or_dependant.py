5cacbe6082f2e6523806b24954f63c93e5ad75f0
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-12-22 19:19:13 -0500

    commit issues fix

--- policyengine_canada/variables/household/person/has_spouse_or_dependant.py
+++
@@ -1,8 +0,0 @@
-from policyengine_canada.model_api import *
-
-
-class has_spouse_or_dependant(Variable):
-    value_type = bool
-    entity = Person
-    label = "Person has a spouse or a dependant"
-    definition_period = YEAR
