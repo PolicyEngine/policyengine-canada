576b26437af8a6a200f06c35d5eeca558f6554a9
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-12-22 19:38:40 -0500

    benefits

--- policyengine_canada/variables/household/income/household/household_net_income.py
+++ policyengine_canada/variables/household/income/household/household_net_income.py
@@ -9,6 +9,22 @@ class household_net_income(Variable):
     definition_period = YEAR
     adds = [
         "individual_net_income",
+        "household_benefits",
+    ]
+
+
+class household_net_income(Variable):
+    value_type = float
+    entity = Household
+    label = "benfits"
+    unit = CAD
+    definition_period = YEAR
+    adds = [
+        "ccb",
+        "cdb",
+        "cwb",
+        "dental_benefit",
+        "old_age_security_pension",
     ]
 
 
