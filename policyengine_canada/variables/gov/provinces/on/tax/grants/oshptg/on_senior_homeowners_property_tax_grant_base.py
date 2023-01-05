4dbb036d943be9fe54755665b6dddb62edb580d0
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-05 14:14:12 -0500

    make format

--- policyengine_canada/variables/gov/provinces/on/tax/grants/oshptg/on_senior_homeowners_property_tax_grant_base.py
+++ policyengine_canada/variables/gov/provinces/on/tax/grants/oshptg/on_senior_homeowners_property_tax_grant_base.py
@@ -11,10 +11,7 @@ class on_senior_homeowners_property_tax_grant_base(Variable):
     def formula(person, period, parameters):
         province = person.household("province", period)
         in_ontario = province == province.possible_values.ONTARIO
-        property_tax_paid = person("property_tax", period)
-        age = person("age", period)
         p = parameters(period).gov.provinces.on.tax.grants.oshptg
-        age_eligibility = p.age_eligibility
-        eligible = property_tax_paid > 0 & age >= age_eligibility & in_ontario
-        max_amount = p.max_amount
-        return eligible * min_(property_tax_paid, max_amount)
+        age_eligible = person("age", period) >= p.age_eligibility
+        eligible = age_eligible & in_ontario
+        return eligible * min_(person("property_tax", period), p.max_amount)
