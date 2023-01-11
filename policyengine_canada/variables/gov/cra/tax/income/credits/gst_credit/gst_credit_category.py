9c66b60ae72cd6e324d7dbfb68af3762737281d3
Alex Rand <alexrand94@gmail.com>
2023-01-11 10:40:10 -0500

    make format

--- policyengine_canada/variables/gov/cra/tax/income/credits/gst_credit/gst_credit_category.py
+++ policyengine_canada/variables/gov/cra/tax/income/credits/gst_credit/gst_credit_category.py
@@ -16,11 +16,24 @@ class gst_credit_category(Variable):
 
     def formula(person, period, parameters):
         return select(
-            [person("is_head", period), person("is_spouse", period), person("is_eldest_child_in_single_household_for_gst_credit", period), person("is_child_for_gst_credit", period) & ~person("is_eldest_child_in_single_household_for_gst_credit", period)],
+            [
+                person("is_head", period),
+                person("is_spouse", period),
+                person(
+                    "is_eldest_child_in_single_household_for_gst_credit",
+                    period,
+                ),
+                person("is_child_for_gst_credit", period)
+                & ~person(
+                    "is_eldest_child_in_single_household_for_gst_credit",
+                    period,
+                ),
+            ],
             [
                 GSTCreditCategory.HEAD,
                 GSTCreditCategory.SPOUSE,
                 GSTCreditCategory.SPOUSE,
                 GSTCreditCategory.CHILD,
-            ], default=0
+            ],
+            default=0,
         )
