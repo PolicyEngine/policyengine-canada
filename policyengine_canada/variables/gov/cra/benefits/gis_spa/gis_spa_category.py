0cdf46e453cf39d80173bbeb7e441a5165135c61
Alex Rand <alexrand94@gmail.com>
2023-01-17 09:27:33 -0500

    Add spouse_oas_eligible.py

--- policyengine_canada/variables/gov/cra/benefits/gis_spa/gis_spa_category.py
+++ policyengine_canada/variables/gov/cra/benefits/gis_spa/gis_spa_category.py
@@ -17,23 +17,24 @@ class gis_spa_category(Variable):
     definition_period = YEAR
 
     def formula(person, period, parameters):
+        household = person.household
         age = person("age", period)
-        married = person("is_spouse", period)
+        married = household("is_married", period)
         widow = person("is_widow", period)
         oas_eligible = person("old_age_security_pension_eligibility", period)
-      #  spouse_oas_eligible = 
+        spouse_oas_eligible = person("spouse_oas_eligible", period)
         p = parameters(period).gov.cra.benefits.gis_spa
         return select(
             [
                 ~married & oas_eligible,
                 # The law specifies widows aged 60-64, but that cutoff of 64 is just because at 65 you become eligible for OAS. I just reference OAS eligibilty directly.
-                widow & ~married & ~oas_eligible & (age >= p.widows_eligibility_age)
-              #  married & oas_eligible & spouse_oas_eligible 
+                widow & ~married & ~oas_eligible & (age >= p.widows_eligibility_age),
+                married & oas_eligible & spouse_oas_eligible 
             ],
             [
                 GISSPACategory.SINGLE_WITH_OAS,
                 GISSPACategory.WIDOW_60_64,
-             #   GISCategory.COUPLE_BOTH_OAS
+                GISSPACategory.COUPLE_BOTH_OAS
             ],
             default=0,
         )
