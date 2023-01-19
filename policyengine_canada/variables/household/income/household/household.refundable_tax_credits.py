bada4177494bcb413613b67995e534c286fc9ee5
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-19 13:16:21 -0500

    variable adjustments

---
+++ policyengine_canada/variables/household/income/household/household.refundable_tax_credits.py
@@ -0,0 +1,13 @@
+from policyengine_canada.model_api import *
+
+
+class household_refundable_tax_credits(Variable):
+    value_type = float
+    entity = Household
+    label = "refundable tax credits"
+    definition_period = YEAR
+    unit = CAD
+    adds = [
+        "refundable_tax_credits",  # Federal.
+        "on_refundable_credits",  # Ontario.
+    ]
