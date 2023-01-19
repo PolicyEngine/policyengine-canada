99490316752517f5c3fdd314b4704b44516d6c5e
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-19 12:24:50 -0500

    Model CWB eligibility at the person level Fixes #54

--- policyengine_canada/variables/gov/cra/benefits/cwb/cwb_eligible.py
+++ policyengine_canada/variables/gov/cra/benefits/cwb/cwb_eligible.py
@@ -3,14 +3,13 @@ from policyengine_canada.model_api import *
 
 class cwb_eligible(Variable):
     value_type = bool
-    entity = Household
+    entity = Person
     label = "Eligible for Canada Workers Benefit"
     definition_period = YEAR
 
-    def formula(household, period, parameters):
-        person = household.members
+    def formula(person, period, parameters):
         p = parameters(period).gov.cra.benefits.cwb
         eligible_age = person("age", period) >= p.eligible_age
-        family = household("is_cwb_family", period)
-        any_of_eligible_age = household.any(eligible_age)
+        family = person.household("is_cwb_family", period)
+        any_of_eligible_age = person.household.any(eligible_age)
         return family | any_of_eligible_age
