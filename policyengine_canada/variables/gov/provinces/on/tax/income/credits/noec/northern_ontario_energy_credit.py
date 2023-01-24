4152e17b39c9dac15e6d974a649dd5863012b424
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-24 18:35:27 -0500

    basic test structure

--- policyengine_canada/variables/gov/provinces/on/tax/income/credits/noec/northern_ontario_energy_credit.py
+++ policyengine_canada/variables/gov/provinces/on/tax/income/credits/noec/northern_ontario_energy_credit.py
@@ -19,24 +19,29 @@ class northern_ontario_energy_credit(Variable):
         p = parameters(period).gov.provinces.on.tax.income.credits.noec
         excess = max_(afni - p.phase_out.start[category], 0)
         phase_out = excess * p.multiplication_factor
-        phased_out = p.base - phase_out
+        print(phase_out)
+        phased_out = p.base[category] - phase_out
         shared_custody_phased_out_divided = (
             phased_out / p.phase_out.shared_custody.divisor
         )
         shared_custody_excess = max_(
-            afni - p.phase_out.shared_custody.start[category], 0
+            afni - p.phase_out.shared_custody.start, 0
         )
-        shared_custody_second_phase_out = shared_custody_excess * p.rate
+        shared_custody_second_phase_out = (
+            shared_custody_excess * p.multiplication_factor
+        )
         shared_custody_second_phased_out = (
             p.phase_out.shared_custody.base - shared_custody_second_phase_out
         )
         shared_custody_second_phased_out_divided = (
             shared_custody_second_phased_out
-            / p.phase_outshared_custody.divisor
+            / p.phase_out.shared_custody.divisor
         )
-        shared_custody_result = (
+        shared_custody_result = where(
+            afni >= p.phase_out.start[category],
             shared_custody_phased_out_divided
-            + shared_custody_second_phased_out_divided
+            + shared_custody_second_phased_out_divided,
+            p.phase_out.shared_custody.base,
         )
         eligible = age >= p.age_eligibility or married == True or children > 0
         return (
