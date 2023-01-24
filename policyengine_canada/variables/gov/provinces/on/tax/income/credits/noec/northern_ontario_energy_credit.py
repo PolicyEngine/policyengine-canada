2903771e5f4e62b42cc553001f117c4034930f09
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-23 19:22:51 -0500

    main variable structure

---
+++ policyengine_canada/variables/gov/provinces/on/tax/income/credits/noec/northern_ontario_energy_credit.py
@@ -0,0 +1,48 @@
+from policyengine_canada.model_api import *
+
+
+class northern_ontario_energy_credit(Variable):
+    value_type = float
+    entity = Household
+    label = "Northern ontario energy credit"
+    unit = CAD
+    definition_period = YEAR
+
+    def formula(household, period, parameters):
+        in_northern_ontario = household("is_in_northern_ontario", period)
+        category = household("oeptc_category", period)
+        afni = household("adjusted_family_net_income", period)
+        p = parameters(period).gov.provinces.on.tax.income.credits.noec
+        excess = max_(afni - p.phase_out.start[category], 0)
+        phase_out = excess * p.rate
+        phased_out = p.base - phase_out
+        shared_custody_phased_out_divided = (
+            phased_out / p.phase_out.shared_custody.divisor
+        )
+        shared_custody_excess = max_(
+            afni - p.phase_out.shared_custody.start[category], 0
+        )
+        shared_custody_second_phase_out = shared_custody_excess * p.rate
+        shared_custody_second_phased_out = (
+            p.phase_out.shared_custody.base - shared_custody_second_phase_out
+        )
+        shared_custody_second_phased_out_divided = (
+            shared_custody_second_phased_out
+            / p.phase_outshared_custody.divisor
+        )
+        shared_custody_result = (
+            shared_custody_phased_out_divided
+            + shared_custody_second_phased_out_divided
+        )
+        return max_(
+            0,
+            where(
+                category == category.possible_values.SINGLE_SHARED_CUSTODY,
+                shared_custody_result,
+                phased_out,
+            ),
+        )
+
+
+# TODO: age  / spouse / parent - eligibility
+# TODO: shared custody under $43k amount is 205.50
