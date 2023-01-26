ebf0d4d88f476af7515e56ad9c09b9cefc4e8796
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-26 11:24:12 -0500

    test and formula adjustment

--- policyengine_canada/variables/gov/provinces/on/benefits/on_trillium_benefit.py
+++ policyengine_canada/variables/gov/provinces/on/benefits/on_trillium_benefit.py
@@ -2,14 +2,10 @@ from policyengine_canada.model_api import *
 
 
 class on_trillium_benefit(Variable):
-    value_type = int
+    value_type = float
     entity = Household
     label = "Ontario trillium benefit"
     documentation = "Sum of the Ontario energy and property tax credit, the Northern Ontario energy credit, and the Ontario sales tax credit programs."
     definition_period = YEAR
 
-    adds = (
-        ["oeptc"],
-        ["on_sales_tax_credit"],
-        ["northern_ontario_energy_credit"],
-    )
+    adds = ["oeptc", "on_sales_tax_credit", "northern_ontario_energy_credit"]
