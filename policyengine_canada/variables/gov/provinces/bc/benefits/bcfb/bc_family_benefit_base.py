accaf5f8b7a3b6695a2d33d7f693a6394b947d91
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-01-27 17:29:41 -0500

    add todo

--- policyengine_canada/variables/gov/provinces/bc/benefits/bcfb/bc_family_benefit_base.py
+++ policyengine_canada/variables/gov/provinces/bc/benefits/bcfb/bc_family_benefit_base.py
@@ -10,8 +10,10 @@ class bc_family_benefit_base(Variable):
 
     def formula(household, period, parameters):
         children = household("bc_family_benefit_eligible_children", period)
-        p = parameters(period).gov.provinces.bc.benefits.bcfb.max_amount
-        return select(
+        p = parameters(period).gov.provinces.bc.benefits.bcfb.base
+        province = household("province", period)
+        in_bc = province == province.possible_values.BRITISH_COLUMBIA
+        return in_bc * select(
             # Conditions.
             [children == 1, children == 2, children > 2],
             # Results.
@@ -22,3 +24,6 @@ class bc_family_benefit_base(Variable):
             ],
             default=0,
         )
+
+
+# TODO: Calculate amounts for each child - first child, second child etc.
