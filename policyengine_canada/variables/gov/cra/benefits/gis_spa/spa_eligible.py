00b3d4ac930aa7d94c4616cd33cc1368bf4425b7
Alex Rand <alexrand94@gmail.com>
2023-02-01 11:00:16 -0500

    fix the unit tests

--- policyengine_canada/variables/gov/cra/benefits/gis_spa/spa_eligible.py
+++ policyengine_canada/variables/gov/cra/benefits/gis_spa/spa_eligible.py
@@ -3,7 +3,7 @@ from policyengine_canada.model_api import *
 class spa_eligible(Variable):
     value_type = bool
     entity = Person
-    label = "Spouse is eligible for the Old Age Security Pension"
+    label = "Person is eligible for the Spousal Allowance"
     definition_period = YEAR
 
     def formula(person, period, parameters):
@@ -13,4 +13,5 @@ class spa_eligible(Variable):
         widow = person("is_widow", period)
         oas_eligible = person("old_age_security_pension_eligibility", period)
         spouse_oas_eligible = person("spouse_oas_eligible", period)
-        return ~oas_eligible & (widow | spouse_oas_eligible) & (age >= p.spa_widows_eligibility_age) & (adult_years_in_canada >= p.spa_residency_requirement)
+        meets_residency_requirement = adult_years_in_canada >= p.spa_residency_requirement
+        return ~oas_eligible & meets_residency_requirement & ((widow & (age >= p.spa_widows_eligibility_age)) | (spouse_oas_eligible & (age >= p.spa_spouse_eligibility_age)))
