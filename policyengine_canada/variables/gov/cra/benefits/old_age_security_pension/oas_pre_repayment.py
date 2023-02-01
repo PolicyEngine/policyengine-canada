5f87aeff0c53e9c1598f1899230042b8f2f94dbf
Alex Rand <alexrand94@gmail.com>
2023-02-01 11:01:17 -0500

    make reformat

--- policyengine_canada/variables/gov/cra/benefits/old_age_security_pension/oas_pre_repayment.py
+++ policyengine_canada/variables/gov/cra/benefits/old_age_security_pension/oas_pre_repayment.py
@@ -2,6 +2,7 @@ from policyengine_canada.model_api import *
 
 # SPSD/M 29.0: imioas or imoasmax, I'm not sure. I think imioas is post-repayment?
 
+
 class oas_pre_repayment(Variable):
     value_type = float
     entity = Person
