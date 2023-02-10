cd80ea8d2b2d469cf0eb5eb6c8319a14d283a747
PavelMakarchuk <pavel.ma99@gmail.com>
2023-02-09 19:01:02 -0500

    Add variable for income tax (after refundable credits) Fixes #136

---
+++ policyengine_canada/variables/gov/cra/tax/income/income_tax_after_refundable_credits.py
@@ -0,0 +1,13 @@
+from policyengine_canada.model_api import *
+
+
+class income_tax_after_refundable_credits(Variable):
+    value_type = float
+    entity = Household
+    label = "Income tax after non-refundable tax credits"
+    unit = CAD
+    documentation = "Example income tax regime"
+    definition_period = YEAR
+
+    adds = ["income_tax_before_refundable_credits"]
+    subtracts = ["refundable_tax_credits"]
