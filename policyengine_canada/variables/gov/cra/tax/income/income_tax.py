4165e3542e65b96a75009f69e28588cae3debae5
Max Ghenis <mghenis@gmail.com>
2023-02-09 23:29:50 -0500

    fix documentation

--- policyengine_canada/variables/gov/cra/tax/income/income_tax.py
+++ policyengine_canada/variables/gov/cra/tax/income/income_tax.py
@@ -4,9 +4,8 @@ from policyengine_canada.model_api import *
 class income_tax(Variable):
     value_type = float
     entity = Household
-    label = "Income tax after non-refundable tax credits"
+    label = "Income tax after credits"
     unit = CAD
-    documentation = "Example income tax regime"
     definition_period = YEAR
 
     adds = ["income_tax_before_refundable_credits"]
