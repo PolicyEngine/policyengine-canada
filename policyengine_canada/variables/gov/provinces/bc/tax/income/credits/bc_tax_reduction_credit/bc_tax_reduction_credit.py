6730bdeabcf5d9db10d95e7cceee051b4943c57e
PavelMakarchuk <pavel.ma99@gmail.com>
2023-02-10 19:09:41 -0500

    non-refundable credits bc

---
+++ policyengine_canada/variables/gov/provinces/bc/tax/income/credits/bc_tax_reduction_credit/bc_tax_reduction_credit.py
@@ -0,0 +1,16 @@
+from policyengine_canada.model_api import *
+
+
+class bc_tax_reduction_credit(Variable):
+    value_type = float
+    entity = Person
+    label = "British Columbia tax reduction credit"
+    unit = CAD
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        income = person("bc_taxable_income", period)
+        p = parameters(
+            period
+        ).gov.provinces.bc.tax.income.credits.bc_tax_reduction_credit
+        return max_(p.base - p.reduction.calc(income), 0)
