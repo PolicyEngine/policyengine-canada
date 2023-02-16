61df5ab2b16792623449ca58b8d664d4518b0557
PavelMakarchuk <pavel.ma99@gmail.com>
2023-02-16 17:07:44 -0500

    variable and test structure

---
+++ policyengine_canada/variables/gov/cra/tax/income/credits/basic_personal_amount/basic_personal_amount.py
@@ -0,0 +1,17 @@
+from policyengine_canada.model_api import *
+
+
+class basic_personal_amount(Variable):
+    value_type = float
+    entity = Person
+    label = "Basic Personal Amount"
+    unit = CAD
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.cra.tax.income.credits.basic_personal_amount
+        supplement = person("basic_personal_amount_supplement", period)
+        return p.base + supplement
+
+
+# TODO: add to the net income tree.
