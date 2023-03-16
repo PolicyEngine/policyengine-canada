bed91c6c0e93095f70e64599b6662da654285f7a
PavelMakarchuk <pavel.ma99@gmail.com>
2023-03-16 17:54:54 -0400

    variable structure

---
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/credits/childrens_arts_credit/childrens_arts_credit.py
@@ -0,0 +1,17 @@
+from policyengine_canada.model_api import *
+
+
+class childrens_arts_credit(Variable):
+    value_type = float
+    entity = Household
+    label = "Yukon childrens arts credit"
+    definition_period = YEAR
+    defined_for = ProvinceCode.YT
+
+    def formula(household, period, parameters):
+        children = household("childrens_arts_credit_eligible_children", period)
+        expenses = household("childrens_arts_credit_expenses", period)
+        p = parameters(
+            period
+        ).gov.provinces.yt.tax.income.credits.childrens_arts_credit
+        return min_(expenses, children * p.amount)
