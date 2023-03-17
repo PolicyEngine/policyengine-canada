10172abfc903bd62a8108b1ae91e661cdb1e8fea
PavelMakarchuk <pavel.ma99@gmail.com>
2023-03-16 20:25:07 -0400

    variable

---
+++ policyengine_canada/variables/gov/provinces/nu/tax/income/credits/nu_cost_of_living_credit.py
@@ -0,0 +1,21 @@
+from policyengine_canada.model_api import *
+
+
+class nu_cost_of_living_credit(Variable):
+    value_type = float
+    entity = Person
+    label = "Nunavut cost of living credit"
+    unit = CAD
+    definition_period = YEAR
+    defined_for = ProvinceCode.NU
+
+    def formula(person, period, parameters):
+        p = parameters(
+            period
+        ).gov.provinces.nu.tax.income.credits.cost_of_living
+        income = person("net_income", period)
+        armed_deduction = person(
+            "canadian_armed_forces_and_personnel_deduction", period
+        )
+        adjusted_income = income - armed_deduction
+        return min_(p.max_amount, adjusted_income * p.rate)
