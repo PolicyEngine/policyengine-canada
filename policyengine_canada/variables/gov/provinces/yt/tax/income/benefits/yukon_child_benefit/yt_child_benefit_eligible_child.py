75db6e68fb147ab8cebf64cce3f8e6e2ab7d9e3e
PavelMakarchuk <pavel.ma99@gmail.com>
2023-03-22 17:48:19 -0400

    variable structure

---
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/benefits/yukon_child_benefit/yt_child_benefit_eligible_child.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class yt_child_benefit_eligible_child(Variable):
+    value_type = bool
+    entity = Person
+    label = "Yukon child benefit eligible child"
+    definition_period = YEAR
+    defined_for = ProvinceCode.YT
+
+    def formula(person, period, parameters):
+        age = person("age", period)
+        return (
+            age
+            < parameters(
+                period
+            ).gov.provinces.yt.benefits.child_benefit.age_eligibility
+        )
---
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/benefits/yukon_child_benefit/yt_child_benefit_post_reduction.py
@@ -0,0 +1,9 @@
+from policyengine_canada.model_api import *
+
+
+class yt_child_benefit_post_redcution(Variable):
+    value_type = bool
+    entity = Household
+    label = "Yukon child benefit post reduction"
+    definition_period = YEAR
+    defined_for = ProvinceCode.YT
