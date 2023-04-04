2407b9a40df424bc873ae02402a35cea35f0c745
PavelMakarchuk <pavel.ma99@gmail.com>
2023-04-04 16:56:53 -0400

    variables

---
+++ policyengine_canada/variables/gov/provinces/nu/tax/income/benefits/nu_child_benefit_base_component_base.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class nu_child_benefit_base_component_base(Variable):
+    value_type = float
+    entity = Household
+    label = "Nunvaut child benefit base component"
+    definition_period = YEAR
+    defined_for = ProvinceCode.NU
+
+    def formula(household, period, parameters):
+        children = household("nu_child_benefit_eligible_children", period)
+        return (
+            children
+            * parameters(
+                period
+            ).gov.provinces.nu.tax.benefits.nucb.base_component.base
+        )
