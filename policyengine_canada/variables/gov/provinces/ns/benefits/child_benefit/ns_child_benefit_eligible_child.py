1700fdc650b857ca59c5cb2c8d82764938f97998
PavelMakarchuk <pavel.ma99@gmail.com>
2023-04-04 18:49:56 -0400

    partial benefit structure

---
+++ policyengine_canada/variables/gov/provinces/ns/benefits/child_benefit/ns_child_benefit_eligible_child.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class ns_child_benefit_eligible_child(Variable):
+    value_type = bool
+    entity = Person
+    label = "Nova Scotia child benefit eligible child"
+    definition_period = YEAR
+    defined_for = ProvinceCode.NS
+
+    def formula(person, period, parameters):
+        age = person("age", period)
+        return (
+            age
+            < parameters(
+                period
+            ).gov.provinces.ns.benefits.child_benifit.age_eligibility
+        )
