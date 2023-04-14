89a811e9d79bddb5266bb28c6abb7d6858c6cf2d
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-04-14 21:25:14 +0000

    Modified base amount

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cost_of_living/cost_of_living_credit.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cost_of_living/cost_of_living_credit.py
@@ -4,7 +4,7 @@ from policyengine_canada.model_api import *
 class cost_of_living_credit(Variable):
     value_type = float
     entity = Person
-    label = "Quebec csot of living credit"
+    label = "Quebec cost of living credit"
     definition_period = YEAR
     defined_for = ProvinceCode.QC
 
@@ -14,5 +14,5 @@ class cost_of_living_credit(Variable):
         ).gov.provinces.qc.tax.income.credits.cost_of_living
         income = person("individual_net_income", period)
         base = person("base", period)
-        
-        return max_(0, base - p.reduction.calc(income))
+
+        return base #max_(0, base - p.reduction.calc(income))
