6b312064c78d971775d48c1e5f54713a96b482f3
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-04-17 23:35:12 +0000

    Quebec cost of living tax credit Fixes #233

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cost_of_living/cost_of_living_credit.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cost_of_living/cost_of_living_credit.py
@@ -13,6 +13,4 @@ class cost_of_living_credit(Variable):
             period
         ).gov.provinces.qc.tax.income.credits.cost_of_living
         income = person("individual_net_income", period)
-        base = person("base", period)
-
-        return max_(0, base - p.reduction.calc(income))
+        return max_(0, p.base - p.reduction.calc(income))
