ef5b016a16c1ce3af4f7a91b9f0f0b764d1dc8c9
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-04-18 01:00:07 +0000

    Variable

---
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credits.py
@@ -0,0 +1,17 @@
+from policyengine_canada.model_api import *
+
+
+class ns_affordable_living_tax_credits(Variable):
+    value_type = float
+    entity = Household
+    label = "Nova Scotia affordable living tax credits"
+    unit = CAD
+    definition_period = YEAR
+    defined_for = ProvinceCode.NS
+
+    def formula(household, period, parameters):
+        income = household("adjusted_family_net_income", period)
+        children = household("count_children", period)
+        p = parameters(period).gov.provinces.ns.tax.income.credits.nsaltc
+        return max_(0, (p.individual_base + (p.child_base * children) ) - p.reduction.calc(income))
+
