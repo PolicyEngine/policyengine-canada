5207c23985c219f58027a3ccb1577e527fe9c7bb
PavelMakarchuk <pavel.ma99@gmail.com>
2023-04-19 17:51:17 -0400

    partial comments

--- policyengine_canada/variables/gov/provinces/sk/benefits/afb/sk_active_family_benefit.py
+++ policyengine_canada/variables/gov/provinces/sk/benefits/afb/sk_active_family_benefit.py
@@ -4,18 +4,21 @@ from policyengine_canada.model_api import *
 class sk_active_family_benefit(Variable):
     value_type = float
     entity = Household
-    label = "Sasktachewan Active Family Benefit"
+    label = "Saskatchewan Active Family Benefit"
     definition_period = YEAR
     defined_for = ProvinceCode.SK
 
     def formula(household, period, parameters):
         income = household("adjusted_family_net_income", period)
-        children_nondisabled = household("sk_afb_eligible_children", period)
-        children_disabled = household("sk_afb_disabled_children", period)
         p = parameters(period).gov.provinces.sk.benefits.afb
-        return where(
-            income > p.income_threshold,
-            0,
-            p.base * children_nondisabled
-            + p.disabled_base * children_disabled,
-        )
+        person = household.members
+        child = person("age", period) <= p.age_eligibility
+        disabled = person("is_disabled", period)
+        children = household.sum(child)
+        disabled_children = household.sum(child & disabled)
+        non_disabled_children = children - disabled_children
+        eligible = income <= p.income_threshold
+        amount_if_eligible = (
+            p.amount.non_disabled * non_disabled_children
+        ) + (p.amount.disabled * disabled_children)
+        return eligible * amount_if_eligible
