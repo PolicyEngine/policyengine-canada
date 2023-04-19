de28dde7aa166b272cd0bd017fc22115380ff87a
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-04-19 22:53:48 +0000

    Test change & Income tree

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
@@ -13,5 +13,6 @@ class ns_affordable_living_tax_credit(Variable):
         income = household("adjusted_family_net_income", period)
         children = household("count_children", period)
         p = parameters(period).gov.provinces.ns.tax.income.credits.nsaltc
-        return max_(0, (p.individual_base + (p.child_base * children) ) - p.reduction.calc(income))
+        child_amount = (p.child_base * children)
+        return max_(0, (p.individual_base + child_amount ) - p.reduction.calc(income))
 
