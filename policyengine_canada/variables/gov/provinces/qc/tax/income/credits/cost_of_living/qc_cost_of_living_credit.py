4d8fe312d601c00852703c6ac48349152eef6002
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-04-19 21:40:51 +0000

    make format

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cost_of_living/qc_cost_of_living_credit.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cost_of_living/qc_cost_of_living_credit.py
@@ -14,15 +14,17 @@ class qc_cost_of_living_credit(Variable):
         ).gov.provinces.qc.tax.income.credits.cost_of_living
         income = person("individual_net_income", period)
 
-        # determine eligibility: 
+        # determine eligibility:
         # You were 18 or over or, if you were under 18, you:
-            # (a) were the father or mother of a child who lived with you; or
-            # (b) were recognized as an emancipated minor by a competent authority (such as a court)
+        # (a) were the father or mother of a child who lived with you; or
+        # (b) were recognized as an emancipated minor by a competent authority (such as a court)
         is_adult = person("is_adult", period)
         is_child = person("is_child", period)
         is_father = person("is_father", period)
         is_mother = person("is_mother", period)
 
-        eligible = is_adult or (is_child and is_father) or (is_child and is_mother)
+        eligible = (
+            is_adult or (is_child and is_father) or (is_child and is_mother)
+        )
 
-        return max_(0, eligible *  (p.base - p.reduction.calc(income)))
+        return max_(0, eligible * (p.base - p.reduction.calc(income)))
