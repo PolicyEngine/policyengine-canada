f0172cf9ed49b651f5e59373c9060ea5fea731c3
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-04-25 03:19:11 +0000

    added childcare expense limit

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cce/cce_eligible_child.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cce/cce_eligible_child.py
@@ -14,14 +14,17 @@ class cce_eligible_child(Variable):
         # the child must meet one of the following conditions:
         # (a) He or she was 16 or under at some point in the year.
         # (b) He or she had an infirmity and was your or your spouse's dependant (regardless of his or her age).
+        # An eligible child can be:
+        # (b1) your or your spouse's child; or
+        # (b2) child who was your or your spouse's dependant and whose income for the year was not more than $11,081.
 
         # (a)
         age = person("age", period)
 
         # (b)
-        # is_dependant = person("is_dependent", period) #!!!! should also considering > 18
-        # is_disabled
+        dependent = person("is_dependent", period)
+        disabled = person("is_disabled", period)
 
-        return age <= p.age_eligibility
+        return (age <= p.age_eligibility) | (dependent & disabled)
 
-        #todo: a child who was your or your spouse's dependant and whose income for the year was not more than $11,081.-> make parameter
+        # todo: a child who was your or your spouse's dependant and whose income for the year was not more than $11,081.-> make parameter
--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cce/cce_tax_credit.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/cce/cce_tax_credit.py
@@ -11,10 +11,9 @@ class cce_tax_credit(Variable):
     def formula(household, period, parameters):
         p = parameters(period).gov.provinces.qc.tax.income.credits.cce
 
-        income = household(
-            "adjusted_family_net_income", period
-        )  # adjusted_family_net_income
+        income = household("adjusted_family_net_income", period)
         eligible = household("cce_eligible_child", period)
+
         return eligible * (p.rate * income)
 
 
