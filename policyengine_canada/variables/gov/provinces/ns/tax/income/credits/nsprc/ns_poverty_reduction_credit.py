fba2f04b269a58425f083adad73262cb0d06cd44
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-04-27 00:08:27 +0000

    Minor adjustment

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsprc/ns_poverty_reduction_credit.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsprc/ns_poverty_reduction_credit.py
@@ -15,6 +15,6 @@ class ns_poverty_reduction_credit(Variable):
     def formula(household, period, parameters):
         income = household("adjusted_family_net_income", period)
         children = household("count_children", period)
-        p = parameters(period).gov.provinces.ns.tax.credits.nsprc
+        p = parameters(period).gov.provinces.ns.tax.income.credits.nsprc
         eligible = (children == 0) & (income < p.income_threshold)
         return where(eligible, p.amount, 0)
