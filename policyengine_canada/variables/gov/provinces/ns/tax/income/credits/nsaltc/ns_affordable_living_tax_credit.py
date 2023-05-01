ca9bc01e89dc154cb5c2f1c1f154bd1dc9c7005e
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-05-01 13:42:04 -0400

    Update policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
@@ -14,6 +14,8 @@ class ns_affordable_living_tax_credit(Variable):
         children = household("count_children", period)
         p = parameters(period).gov.provinces.ns.tax.income.credits.nsaltc
         child_amount = p.child_base * children
-        return max_(
+        max_amount = p.individual_base + child_amount
+        reduction = p.reduction.calc(income)
+        return max_(max_amount - reduction, 0)
             0, (p.individual_base + child_amount) - p.reduction.calc(income)
         )
