f46c7d48a8889157c6d17e9179e47d76f1f40fce
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-05-01 17:54:09 +0000

    Fix lables and descriptions

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/nsaltc/ns_affordable_living_tax_credit.py
@@ -4,7 +4,7 @@ from policyengine_canada.model_api import *
 class ns_affordable_living_tax_credit(Variable):
     value_type = float
     entity = Household
-    label = "Nova Scotia affordable living tax credits"
+    label = "NSALTC"
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.NS
@@ -17,5 +17,3 @@ class ns_affordable_living_tax_credit(Variable):
         max_amount = p.individual_base + child_amount
         reduction = p.reduction.calc(income)
         return max_(max_amount - reduction, 0)
-            0, (p.individual_base + child_amount) - p.reduction.calc(income)
-        )
