ae3d864ca0b8c012c20d3daac5994f00918646fc
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-05-07 19:40:31 -0400

    variable adjustment and make format

--- policyengine_canada/variables/gov/provinces/ns/benefits/child_benefit/ns_child_benefit.py
+++ policyengine_canada/variables/gov/provinces/ns/benefits/child_benefit/ns_child_benefit.py
@@ -13,11 +13,14 @@ class ns_child_benefit(Variable):
         p = parameters(period).gov.provinces.ns.benefits.child_benefit
         income = household("adjusted_family_net_income", period)
         eligible = income < p.upper_threshold
+        # In the lower income bracket, the families are eligible to receive $1,275 per child.
+        amount_per_child = children * p.base
+        # In the higher income bracket, families are eligible to receive $1,275 for the first born child.
+        higher_income_base = p.base * (children > 0)
+        # In the higher income bracket, families are eligible to $637.50 per child after the first child.
+        higher_income_additional_child = p.higher_income_base * (children - 1)
         return eligible * where(
             income < p.lower_threshold,
-            children * p.base,
-            (
-                (p.base * (children > 0))
-                + (p.higher_income_base * (children - 1))
-            ),
+            amount_per_child,
+            ((higher_income_base) + (higher_income_additional_child)),
         )
