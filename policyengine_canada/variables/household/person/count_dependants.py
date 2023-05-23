fa9cdaef90c7e53e06ff3ba8f6613f07c047128c
7qiwx <qiwx@uw.edu>
2023-05-23 16:05:15 -0700

    Alberta Marital Status Tax Credit Fixes #283
    Co-authored-by: oliviacaiww <oliviacaiww@users.noreply.github.com>
    Co-authored-by: PavelMakarchuk <PavelMakarchuk@users.noreply.github.com>

---
+++ policyengine_canada/variables/household/person/count_dependants.py
@@ -0,0 +1,10 @@
+from policyengine_canada.model_api import *
+
+
+class count_dependants(Variable):
+    value_type = int
+    entity = Household
+    label = "Dependannts"
+    unit = CAD
+    documentation = "Number of dependants"
+    definition_period = YEAR
