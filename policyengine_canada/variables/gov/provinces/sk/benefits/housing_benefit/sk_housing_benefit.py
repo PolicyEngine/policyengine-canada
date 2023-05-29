4cfd354d2fcf60985a9bbd31500c20181aa7d561
Yaohhhh <128541260+Yaohhhh@users.noreply.github.com>
2023-05-29 21:49:24 +0000

    Errors fixed and pass all tests.
    Co-authored-by: LanxueHe <Snownow0w@users.noreply.github.com>
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_canada/variables/gov/provinces/sk/benefits/housing_benefit/sk_housing_benefit.py
+++ policyengine_canada/variables/gov/provinces/sk/benefits/housing_benefit/sk_housing_benefit.py
@@ -14,32 +14,45 @@ class sk_housing_benefit(Variable):
         person = household.members
         rent = person("rent", period)
         household_rent = household.sum(rent)
-        dependants = household("count_dependants",period)
+        dependants = household("count_dependants", period)
         utility_costs = household("utilities_costs", period)
         precentage_spend_housing = (household_rent + utility_costs) / income
         p = parameters(period).gov.provinces.sk.benefits.housing_benefit
 
-        shelter_costs_lower_bracket = (p.amount.higher_threshold.threshold >= precentage_spend_housing >= p.amount.lower_threshold.threshold)
-        shelter_costs_higher_bracket = (precentage_spend_housing > p.amount.higher_threshold.threshold)
-        # If a household spends between 45% and 35% of their income on housing expenditures 
-        # it is eligible for a benefit amount depending on the number of dependants
-        lower_bracket_amount = shelter_costs_lower_bracket * (select(
-            [dependants == 0, dependants == 1, dependants >= 2],
-            [p.amount.lower_threshold.no_dependants, p.amount.lower_threshold.one_dependants, p.amount.lower_threshold.two_or_more_dependants])
+        shelter_costs_lower_bracket = (
+            p.amount.higher_threshold.threshold
+            >= precentage_spend_housing
+            >= p.amount.lower_threshold.threshold
         )
-        # If a household spends over 45% of their income on housing expenditures 
+        shelter_costs_higher_bracket = (
+            precentage_spend_housing > p.amount.higher_threshold.threshold
+        )
+        # If a household spends between 45% and 35% of their income on housing expenditures
+        # it is eligible for a benefit amount depending on the number of dependants
+        lower_bracket_amount = shelter_costs_lower_bracket * (
+            select(
+                [dependants == 0, dependants == 1, dependants >= 2],
+                [
+                    p.amount.lower_threshold.no_dependants,
+                    p.amount.lower_threshold.one_dependants,
+                    p.amount.lower_threshold.two_or_more_dependants,
+                ],
+            )
+        )
+        # If a household spends over 45% of their income on housing expenditures
         # it is eligible for a higher benefit amount depending on the number of dependants
-        higher_bracket_amount = shelter_costs_higher_bracket * (select(
-            [
-                dependants == 0, 
-                dependants == 1, 
-                dependants >= 2
-            ],
-            [
-                p.amount.higher_threshold.no_dependants,
-                p.amount.higher_threshold.one_dependants,
-                p.amount.higher_threshold.two_or_more_dependants
-            ]))
+        higher_bracket_amount = shelter_costs_higher_bracket * (
+            select(
+                [dependants == 0, dependants == 1, dependants >= 2],
+                [
+                    p.amount.higher_threshold.no_dependants,
+                    p.amount.higher_threshold.one_dependants,
+                    p.amount.higher_threshold.two_or_more_dependants,
+                ],
+            )
+        )
 
-        sk_housing_benefit = eligible * (higher_bracket_amount + lower_bracket_amount)
+        sk_housing_benefit = eligible * (
+            higher_bracket_amount + lower_bracket_amount
+        )
         return sk_housing_benefit
--- policyengine_canada/variables/gov/provinces/sk/benefits/housing_benefit/sk_housing_benefit_eligible.py
+++ policyengine_canada/variables/gov/provinces/sk/benefits/housing_benefit/sk_housing_benefit_eligible.py
@@ -14,45 +14,49 @@ class sk_housing_benefit_eligible(Variable):
         income = household("adjusted_family_net_income", period)
         rent = person("rent", period)
         household_rent = household.sum(rent)
-        dependants = household("count_dependants",period)
+        dependants = household("count_dependants", period)
         utility_costs = household("utilities_costs", period)
-        #1 spend on rent and utilities >= 35% annual before tax household income
+        # 1 spend on rent and utilities >= 35% annual before tax household income
         precentage_spend_housing = (household_rent + utility_costs) / income
-        shelter_costs_eligibility = (precentage_spend_housing >= p.amount.lower_threshold.threshold)
-        #2 have < 300000 in household assets
-        household_assets_eligibility = (household("household_assets",period) < p.housing_assets_limit)
-        #3 annual before-tax household income requirement within its limitation
-        annual_income_threshold = select(
-            [
-                dependants == 0, 
-                dependants == 1, 
-                dependants >= 2
-        ],
-        [
-            p.income_limit.no_dependants,  
-            p.income_limit.one_dependant, 
-            p.income_limit.two_or_more_dependant
-            ], 
+        shelter_costs_eligibility = (
+            precentage_spend_housing >= p.amount.lower_threshold.threshold
         )
-        income_eligibility = (income <= annual_income_threshold)
-        
-        #4 Not receive support from another Government of Saskatchewan income assistance or training program;
-        sk_income_assitance = household("sk_income_assistance", period)
-        income_assistance_eligibility = (sk_income_assitance == 0)
+        # 2 have < 300000 in household assets
+        household_assets_eligibility = (
+            household("household_assets", period) < p.housing_assets_limit
+        )
+        # 3 annual before-tax household income requirement within its limitation
+        annual_income_threshold = select(
+            [dependants == 0, dependants == 1, dependants >= 2],
+            [
+                p.income_limit.no_dependants,
+                p.income_limit.one_dependant,
+                p.income_limit.two_or_more_dependant,
+            ],
+        )
+        income_eligibility = income <= annual_income_threshold
 
-        #5 Not rent from a housing authority under the Social Housing Program
+        # 4 Not receive support from another Government of Saskatchewan income assistance or training program;
+        sk_income_assitance = household("sk_income_assistance", period)
+        income_assistance_eligibility = sk_income_assitance == 0
+
+        # 5 Not rent from a housing authority under the Social Housing Program
         person = household.members
         sk_social_housing_program = person("sk_social_housing_program", period)
         sk_social_housing_programs = household.any(sk_social_housing_program)
-        social_housing_eligibility = (sk_social_housing_programs == 0)
+        social_housing_eligibility = sk_social_housing_programs == 0
 
-        #6 Not a full-time post-secondary student
+        # 6 Not a full-time post-secondary student
         student = person("is_full_time_student", period)
         students = household.any(student)
-        student_eligibility = (students == 0)
+        student_eligibility = students == 0
 
-        sk_housing_benefit_eligible = (shelter_costs_eligibility & household_assets_eligibility & income_eligibility & income_assistance_eligibility & social_housing_eligibility & student_eligibility)
+        sk_housing_benefit_eligible = (
+            shelter_costs_eligibility
+            & household_assets_eligibility
+            & income_eligibility
+            & income_assistance_eligibility
+            & social_housing_eligibility
+            & student_eligibility
+        )
         return sk_housing_benefit_eligible
-
-
-       
