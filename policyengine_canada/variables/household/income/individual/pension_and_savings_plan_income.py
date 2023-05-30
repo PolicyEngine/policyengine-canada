55a603130be74a7631f781d6a458fb6f07b22ddb
PavelMakarchuk <pavel.ma99@gmail.com>
2023-05-30 01:46:20 -0400

    format

--- policyengine_canada/variables/household/income/individual/pension_and_savings_plan_income.py
+++ policyengine_canada/variables/household/income/individual/pension_and_savings_plan_income.py
@@ -4,7 +4,7 @@ from policyengine_canada.model_api import *
 class pension_and_savings_plan_income(Variable):
     value_type = float
     entity = Person
-    label = "Pension and saving plan income Income"
+    label = "Pension and Savings Plan Income"
     unit = CAD
-    documentation = "Income from pension and savings plans"
+    documentation = "Income from pensions and savings plans"
     definition_period = YEAR
