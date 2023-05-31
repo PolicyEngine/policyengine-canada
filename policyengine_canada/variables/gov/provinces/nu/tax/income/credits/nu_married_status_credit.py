53621ab8974c3e1fb6e8793d292161ab0e50bd1d
runyao-yin <118035310+runyao-yin@users.noreply.github.com>
2023-05-31 00:56:00 +0000

    Fixes issue 314

--- policyengine_canada/variables/gov/provinces/nu/tax/income/credits/nu_married_status_credit.py
+++ policyengine_canada/variables/gov/provinces/nu/tax/income/credits/nu_married_status_credit.py
@@ -1,7 +1,6 @@
 from policyengine_canada.model_api import *
 
-
-class nu_marries_status_credit(Variable):
+class nu_married_status_credit(Variable):
     value_type = float
     entity = Household
     label = "Nunavut married status credit"
@@ -13,8 +12,9 @@ class nu_marries_status_credit(Variable):
         p = parameters(
             period
         ).gov.provinces.nu.tax.income.credits.married_status_credit
-        eligible = age > p.age_eligibility
-        base = p.base
-        spouse = person("is_spouse", period)*person("is_dependent", period)
-        
-        
+        spouse = person("is_spouse", period)
+        dependent = person("is_dependant", period)
+        eligible = household.any(spouse & dependent)
+        income = eligible * person("individual_net_income", period)
+        eligible_income = household.sum(income)
+        return (p.base + max_(0, p.addon_max_amount - eligible_income))*eligible  
--- policyengine_canada/variables/household/person/income.py
+++
@@ -1,9 +0,0 @@
-from policyengine_canada.model_api import *
-
-
-class income(Variable):
-    value_type = float
-    entity = Person
-    label = "Personal income"
-    unit = CAD
-    definition_period = YEAR
