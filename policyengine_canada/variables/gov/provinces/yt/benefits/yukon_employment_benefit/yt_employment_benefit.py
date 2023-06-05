79f1e4f9669f78b52a5a4d73357b57b5ac68b928
sqw0418 <97478242+sqw0418@users.noreply.github.com>
2023-06-05 22:14:51 +0000

    fix issues 285

--- policyengine_canada/variables/gov/provinces/yt/benefits/yukon_employment_benefit/yt_employment_benefit.py
+++ policyengine_canada/variables/gov/provinces/yt/benefits/yukon_employment_benefit/yt_employment_benefit.py
@@ -11,9 +11,10 @@ class yt_employment_benefit(Variable):
     reference = "https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/deductions-credits-expenses/line-31260-canada-employment-amount.html"
 
     def formula(household, period, parameters):
-        income = parameters(period).gov.provinces.yt.benefits.employment_benefit
-         
+        income = parameters(
+            period
+        ).gov.provinces.yt.benefits.employment_benefit
+
         benefits_income = household("benefits_income", period)
 
         return min_(benefits_income, income)
-
