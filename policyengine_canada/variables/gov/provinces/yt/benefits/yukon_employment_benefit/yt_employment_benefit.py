fabc5e88df78cf3063c16f40af452d542d590156
sqw0418 <97478242+sqw0418@users.noreply.github.com>
2023-06-05 22:25:40 +0000

    modify 285

--- policyengine_canada/variables/gov/provinces/yt/benefits/yukon_employment_benefit/yt_employment_benefit.py
+++ policyengine_canada/variables/gov/provinces/yt/benefits/yukon_employment_benefit/yt_employment_benefit.py
@@ -8,13 +8,13 @@ class yt_employment_benefit(Variable):
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.YT
-    reference = "https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/deductions-credits-expenses/line-31260-canada-employment-amount.html"
+    reference = "https://www.canada.ca/en/revenue-agency/services/forms-publications/tax-packages-years/general-income-tax-benefit-package/yukon/5011-pc/information-residents-yukon.html#P4_58310"
 
-    def formula(household, period, parameters):
-        income = parameters(
+    def formula(person, period, parameters):
+        maximun_return_amount = parameters(
             period
         ).gov.provinces.yt.benefits.employment_benefit
 
-        benefits_income = household("benefits_income", period)
+        employment_income = person("employment_income", period)
 
-        return min_(benefits_income, income)
+        return min_(employment_income, maximun_return_amount)
