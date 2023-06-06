41512cad5c45a4cf94eea66f1e3e5e31a8afa94a
Kun Yao <90804230+Kenyaokun@users.noreply.github.com>
2023-06-06 23:56:06 +0000

    Newfoundland and Labrador Seniors’ Benefit First version #308
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_canada/variables/gov/provinces/nl/tax/income/benefits/nl_seniors_benefit.py
+++ policyengine_canada/variables/gov/provinces/nl/tax/income/benefits/nl_seniors_benefit.py
@@ -10,19 +10,25 @@ class nl_seniors_benefit(Variable):
 
     def formula(household, period, parameters):
         person = household.members
-        p = parameters(
-            period
-        ).gov.provinces.nl.tax.income.benefits
+        p = parameters(period).gov.provinces.nl.tax.income.benefits
 
         senior_eligibility = person("age", period) >= p.age_eligibility
+        age_eligible = household.any(senior_eligibility)
 
         # Calculate the senior's income & their spouses' income
-        spouse_income = person("spouse_income", period) * person("is_spouse", period)
+        spouse_income = person("spouse_income", period) * person(
+            "is_spouse", period
+        )
         personal_income = person("individual_net_income", period)
-        total_family_income = spouse_income + personal_income
+        total_family_income = household.sum(spouse_income + personal_income)
 
-        income_eligibility = (total_family_income > p.lower_income_threshold) & (total_family_income < p.upper_income_threshold)
+        income_eligibility = total_family_income < p.upper_income_threshold
 
-        senior_benefit = senior_eligibility * ( p.max_amount - income_eligibility * p.rate * (total_family_income - p.lower_income_threshold))
+        senior_benefit = income_eligibility * (
+            p.max_amount
+            - income_eligibility
+            * p.rate
+            * (total_family_income - p.lower_income_threshold)
+        )
 
-        return senior_benefit
+        return min_(age_eligible * senior_benefit, p.max_amount)
