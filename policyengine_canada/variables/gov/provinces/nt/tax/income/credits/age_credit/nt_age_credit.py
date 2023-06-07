49aa8aa9ddc2bb2753fb7597d244e85e8be1488c
Ziyu Wang <70183694+zqw5219@users.noreply.github.com>
2023-06-07 00:06:32 +0000

    Northwest territories age credit Fixes #288
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>
    Co-authored-by: Kun Yao <Kenyaokun@users.noreply.github.com>

---
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/credits/age_credit/nt_age_credit.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class nt_age_credit(Variable):
+    value_type = float
+    entity = Person
+    label = "Northwest Territories age credit"
+    unit = CAD
+    definition_period = YEAR
+    defined_for = ProvinceCode.NT
+    reference = "https://www.justice.gov.nt.ca/en/files/legislation/income-tax/income-tax.a.pdf"
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.provinces.nt.tax.income.credits.age_credit
+        income = person("nt_taxable_income", period)
+        age = person("age", period)
+        eligible = age >= p.age_eligibility  # this is a bool
+        return eligible * max_((p.max_amount - p.reduction.calc(income)), 0)
