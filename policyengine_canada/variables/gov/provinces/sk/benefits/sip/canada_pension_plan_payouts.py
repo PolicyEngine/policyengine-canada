699e49f8cf41697a8f7c3d4e014873a7e17776f8
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-06-07 21:11:26 +0000

    Added parameter and fixed variables. Co-authored-by: Yaohhhh <Yaohhhh@users.noreply.github.com>

--- policyengine_canada/variables/gov/provinces/sk/benefits/sip/canada_pension_plan_payouts.py
+++ policyengine_canada/variables/gov/provinces/sk/benefits/sip/canada_pension_plan_payouts.py
@@ -2,7 +2,7 @@ from policyengine_canada.model_api import *
 
 
 class canada_pension_plan_payout(Variable):
-    value_type = bool
+    value_type = float
     entity = Person
     label = "Canada Pension Plan yearly payout"
     definition_period = YEAR
--- policyengine_canada/variables/gov/provinces/sk/benefits/sip/sk_seniors_income_plan_in_special_care_home.py
+++ policyengine_canada/variables/gov/provinces/sk/benefits/sip/sk_seniors_income_plan_in_special_care_home.py
@@ -11,6 +11,7 @@ class sk_seniors_income_plan_in_special_care_home(Variable):
     def formula(household, period, parameters):
         p = parameters(period).gov.provinces.sk.benefits.sip
         income = household("adjusted_family_net_income", period)
+        cpp = household("canada_pension_plan_payout", period)
         person = household.members
         age = person("age", period)
         eligible = (person("special_care_home", period))
@@ -19,15 +20,19 @@ class sk_seniors_income_plan_in_special_care_home(Variable):
         count_pensioners = household.sum(pensioner)
         spouse_ineligible = person("is_spouse", period) & (age < p.age.spouse_threshold)
         received_allowance = person("receive_allowance")
+        gis = person("gis_reduction", period)
         return eligible * (select(
             # Conditions.
-            [(~married) & (pensioner == true), (married) & (count_pensioners == 2), (married) & spouse_ineligible, (married) & (received_allowance > 0)],
+            [(~married) & (pensioner == true) & (cpp < p.living_in_special_care_home.cpp_income.single) & (income < p.living_in_special_care_home.annual_income_limit.single), 
+             (married) & (count_pensioners == 2) & (cpp < p.living_in_special_care_home.cpp_income.married_both_pensioners) & (income < p.living_in_special_care_home.annual_income_limit.married_both_pensioners), 
+             (married) & spouse_ineligible & (cpp < p.living_in_special_care_home.cpp_income.married_spouse_below_age_threshold) & (income < p.living_in_special_care_home.annual_income_limit.married_spouse_below_age_threshold), 
+             (married) & (received_allowance > 0) & (cpp < p.living_in_special_care_home.cpp_income.married_spouse_receiving_allowance) & (income < p.living_in_special_care_home.annual_income_limit.married_spouse_receiving_allowance)],
             # Results.
             [
-                p.living_in_special_care_home.max_amount.single, #need to add reduction & CPP eligibility,
-                p.living_in_special_care_home.max_amount.married_both_pensioners,
-                p.living_in_special_care_home.max_amount.married_spouse_below_age_threshold,
-                p.living_in_special_care_home.max_amount.married_spouse_receiving_allowance,
+                p.living_in_special_care_home.max_amount.single - gis * p.living_in_special_care_home.reduction_rate.single, 
+                p.living_in_special_care_home.max_amount.married_both_pensioners - gis * p.living_in_special_care_home.reduction_rate.married_both_pensioners,
+                p.living_in_special_care_home.max_amount.married_spouse_below_age_threshold - gis * p.living_in_special_care_home.reduction_rate.married_spouse_below_age_threshold,
+                p.living_in_special_care_home.max_amount.married_spouse_receiving_allowance - gis * p.living_in_special_care_home.reduction_rate.married_spouse_receiving_allowance,
             ],
             default=0,
         ))
