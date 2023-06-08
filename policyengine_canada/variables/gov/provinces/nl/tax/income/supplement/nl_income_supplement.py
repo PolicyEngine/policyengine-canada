bd619aa8ed88f7df5186f592d9cae22aa642ab85
Kun Yao <90804230+Kenyaokun@users.noreply.github.com>
2023-06-08 02:29:04 +0000

    Newfoundland and Labrador Income Supplement Modify #300
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_canada/variables/gov/provinces/nl/tax/income/supplement/nl_income_supplement.py
+++ policyengine_canada/variables/gov/provinces/nl/tax/income/supplement/nl_income_supplement.py
@@ -12,25 +12,27 @@ class nl_income_supplement(Variable):
     def formula(household, period, parameters):
         person = household.members
         p = parameters(period).gov.provinces.nl.tax.income.supplement.income_supplement
-
+        age = person("age", period)
         # Get the income
-        spouse_income = person("spouse_income", period) * person("is_spouse", period)
-        personal_income = person("individual_net_income", period)
+        spouse_income = person("spouse_income", period)
+        personal_income = person("individual_net_income", period) * person("is_head", period)
         total_family_income = household.sum(spouse_income + personal_income)
-
+        # Get an amount for spouse
         spouse_supplement = person("is_spouse", period) * p.spouse_amount
-
-        basic_income_supplement = (p.basic_credit + spouse_supplement) * (total_family_income <= p.phase_in_income_threshold)
-        
-        phase_in_income_supplement = (total_family_income - p.lower_income_threshold) * p.phase_in_rate * ((total_family_income > p.lower_income_threshold) & (total_family_income <= p.phase_in_income_threshold))
-        
-        max_amount = (p.maximum_credit + spouse_supplement) * (total_family_income > p.phase_in_income_threshold)
-        
-        phase_out_income_supplement = (total_family_income - p.phase_out_income_threshold) * p.phase_out_rate * (total_family_income > p.phase_out_income_threshold)
-        
-        total_income_supplement = household.max(basic_income_supplement + phase_in_income_supplement + max_amount - phase_out_income_supplement)
-
-        return max_(total_income_supplement, 0)
+        # Base amount for self and spouse
+        self_and_spouse_credit = p.basic_credit + spouse_supplement
+        # Households can get an additional amount per child udner 19
+        age_eligible = age < p.child_age_eligibility
+        child_amount = household.sum(age_eligible) * p.child_amount
+        # Households can receive an additional amount if received the disability tax credit
+        disability_credit = person("disability_tax_credit", period)
+        disability_amount = household.any(disability_credit) * p.disability_amount
+        # Supplement phases in at 5.32% between $15,000 and $20,000 of income maxed at $266
+        phased_in_income_supplement = min_(p.phase_in_rate.calc(total_family_income), p.income_supplement_max_amount)
+        # Maximum credit
+        max_amount = self_and_spouse_credit + phased_in_income_supplement + disability_amount + child_amount
+        # Amount phases out at 9% over $40,000
+        return max_(max_amount - p.phase_out_rate.calc(total_family_income), 0)
         
 
 
