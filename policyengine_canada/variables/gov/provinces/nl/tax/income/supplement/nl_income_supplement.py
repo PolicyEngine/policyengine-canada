970c7d36c2b4360a8291cdff727510e17b662d41
Kun Yao <90804230+Kenyaokun@users.noreply.github.com>
2023-06-08 00:18:08 +0000

    Newfoundland and Labrador Income Supplement Second trial - with spouse #300

--- policyengine_canada/variables/gov/provinces/nl/tax/income/supplement/nl_income_supplement.py
+++ policyengine_canada/variables/gov/provinces/nl/tax/income/supplement/nl_income_supplement.py
@@ -20,13 +20,15 @@ class nl_income_supplement(Variable):
 
         spouse_supplement = person("is_spouse", period) * p.spouse_amount
 
-        # children_amount = person("own_children_in_household") * p.children_amount
-
-        basic_income_supplement = (p.basic_credit + spouse_supplement) * (total_family_income <= p.lower_income_threshold)
-        phase_in_income_supplement = (total_family_income - p.lower_income_threshold) * p.phase_in_rate * ((total_family_income > p.lower_income_threshold) & (total_family_income <= p.upper_income_threshold))
-        phase_out_income_supplement = (p.maximum_credit + spouse_supplement - (total_family_income - p.upper_income_threshold) * p.phase_out_rate * (total_family_income > p.upper_income_threshold)) * (total_family_income > p.upper_income_threshold)
-
-        total_income_supplement = basic_income_supplement + phase_in_income_supplement + phase_out_income_supplement
+        basic_income_supplement = (p.basic_credit + spouse_supplement) * (total_family_income <= p.phase_in_income_threshold)
+        
+        phase_in_income_supplement = (total_family_income - p.lower_income_threshold) * p.phase_in_rate * ((total_family_income > p.lower_income_threshold) & (total_family_income <= p.phase_in_income_threshold))
+        
+        max_amount = (p.maximum_credit + spouse_supplement) * (total_family_income > p.phase_in_income_threshold)
+        
+        phase_out_income_supplement = (total_family_income - p.phase_out_income_threshold) * p.phase_out_rate * (total_family_income > p.phase_out_income_threshold)
+        
+        total_income_supplement = household.max(basic_income_supplement + phase_in_income_supplement + max_amount - phase_out_income_supplement)
 
         return max_(total_income_supplement, 0)
         
