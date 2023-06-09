d8b687957fdb44a60c814c40f56a2204aad85086
Kun Yao <90804230+Kenyaokun@users.noreply.github.com>
2023-06-09 13:57:57 +0000

    Alberta Caregiver Amount Debug #331

--- policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
@@ -15,10 +15,10 @@ class ab_caregiver_amount(Variable):
         p = parameters(period).gov.provinces.ab.tax.income.credits.credits_return
 
         # Whether the person is dependant (65 or older/ disabled)
-        is_dependant = person("is_parent_or_grandparent", period) | (person("age", period) >= 18 & person("is_disabled", period))
+        is_dependant = (person("is_parent_or_grandparent", period) | (person("age", period) >= p.lower_age_eligibility & person("is_disabled", period)))
         dependant_net_income = person("individual_net_income", period) * is_dependant
 
-        income_eligibility = (dependant_net_income >= p.lower_dependent_income_threshold & dependant_net_income <= p.upper_dependent_income_threshold)
-        caregiver_amount = income_eligibility * (p.upper_dependent_income_threshold - dependant_net_income)
+        income_eligibility = dependant_net_income <= p.upper_dependant_income_threshold
+        caregiver_amount = (p.upper_dependant_income_threshold - dependant_net_income) * income_eligibility
 
         return min_(caregiver_amount, p.maximum_caregiver_amount)
