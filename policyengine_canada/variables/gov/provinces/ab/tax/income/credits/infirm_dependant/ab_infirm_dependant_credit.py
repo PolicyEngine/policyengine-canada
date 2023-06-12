d9a9706bf814c24eaafe11feae82c32c63210694
oliviacaiww <oliviacaiww@gmail.com>
2023-06-12 19:43:01 +0800

    changes

--- policyengine_canada/variables/gov/provinces/ab/tax/income/credits/infirm_dependant/ab_infirm_dependant_credit.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/income/credits/infirm_dependant/ab_infirm_dependant_credit.py
@@ -10,10 +10,16 @@ class ab_infirm_dependant_credit(Variable):
 
     def formula(household, period, parameters):
         person = household.members
-        p = parameters(period).gov.provinces.ab.tax.income.credits.infirm_dependant
+        p = parameters(
+            period
+        ).gov.provinces.ab.tax.income.credits.infirm_dependant
         eligible = person("is_infirm_dependant", period)
-        infirm_dependant_income = person("infirm_dependant_income",period)
+        infirm_dependant_income = person("infirm_dependant_income", period)
         income_level = infirm_dependant_income > p.low_income
-        per_credit = where(income_level, max_(0, eligible * (20_190 - infirm_dependant_income)), eligible * p.base)
+        per_credit = where(
+            income_level,
+            max_(0, eligible * (20_190 - infirm_dependant_income)),
+            eligible * p.base,
+        )
         total = household.sum(per_credit)
         return total
