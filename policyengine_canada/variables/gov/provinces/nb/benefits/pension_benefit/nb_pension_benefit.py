f678b70ea77ccb0c151e9d520e74b855004120e1
sakibniaz <124746632+sakibniaz@users.noreply.github.com>
2023-06-15 22:20:18 +0000

    Variable adjustment

--- policyengine_canada/variables/gov/provinces/nb/benefits/pension_benefit/nb_pension_benefit.py
+++ policyengine_canada/variables/gov/provinces/nb/benefits/pension_benefit/nb_pension_benefit.py
@@ -7,14 +7,13 @@ class nb_pension_benefit(Variable):
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.NB
-    adds = "gov.provinces.nb.benefits.benefits"
      
     def formula(person, period, parameters):
         age = person("age", period)
         pension_income = person("pension_and_savings_plan_income", period)
         threshold = parameters(period).gov.provinces.nb.benefits.pension_benefit.age_eligibility
+        maximum_benefit = parameters(period).gov.cra.provinces.nb.benefits.pension_benefit.maximum_benefit
         age_eligibility = age > threshold
-
-      #min of 1_000 or less    
-    return min_(parameters(period).gov.cra.provinces.nb.benefits.pension_benefit.maximum_benefit, age_eligibility * pension_income)
+        #min of 1_000 or less    
+        return age_eligibility * min_(maximum_benefit, pension_income)
     
