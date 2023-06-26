960344a9d1567f97803312a8072bda8c9d1b0f34
RuoqiTan <97366958+RuoqiTan@users.noreply.github.com>
2023-06-26 08:15:48 +0000

    parameter & variables

---
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/credits/yt_basic_personal_amount.py
@@ -0,0 +1,19 @@
+from policyengine_canada.model_api import *
+
+
+class yt_basic_personal_amount(Variable):
+    value_type = float
+    entity = Person
+    label = "Yukon basic personal amount"
+    definition_period = YEAR
+    defined_for = ProvinceCode.YT
+
+    def formula(person, period, parameters):
+        income = person("individual_net_income", period)
+        p = parameters(
+            period
+        ).gov.provinces.yt.tax.income.credits.basic_personal_amount
+        exceedance = income - p.income_threshold
+        eligible = (exceedance >= 0)
+        percent = max_(0, (p.scale_value - exceedance)/p.scale_value)
+        return (percent * p.applicable_amount + p.base_amount) * eligible
