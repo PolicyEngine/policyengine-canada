4f91c74f66361426ff493a6e9ecc958e3b994e2e
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-06-27 05:44:10 +0000

    Resolved Conversations

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/sk_pension_income_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/sk_pension_income_credit.py
@@ -1,18 +1,16 @@
 from policyengine_canada.model_api import *
 
 
-class sk_pension_income_amount(Variable):
+class sk_pension_income_credit(Variable):
     value_type = float
     entity = Person
-    label = "Saskatchewan pension income maximun amount"
+    label = "Saskatchewan pension income credit"
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.SK
 
     def formula(person, period, parameters):
-        p = parameters(
-            period
-        ).gov.provinces.sk.tax.income.credits.pension_income_amount
+        p = parameters(period).gov.provinces.sk.tax.income.credits.pension_income_amount
         pension_payments = person("pension_and_savings_plan_income", period)
         max_amount = p.maximum_amount
         return min_(pension_payments, max_amount)
