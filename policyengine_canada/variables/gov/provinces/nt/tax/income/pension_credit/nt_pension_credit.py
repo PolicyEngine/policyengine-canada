33480790361a9b83aff5bd765a64f67f1b56a5b9
Ziyu Wang <70183694+zqw5219@users.noreply.github.com>
2023-06-29 21:58:25 +0000

    Northwest Territories Pension Credit Fixes #339

---
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/pension_credit/nt_pension_credit.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class nt_pension_credit(Variable):
+    value_type = float
+    entity = Person
+    label = "Northwest Territories pension credit"
+    unit = CAD
+    definition_period = YEAR
+    defined_for = ProvinceCode.NT
+    reference = "https://www.justice.gov.nt.ca/en/files/legislation/income-tax/income-tax.a.pdf"
+
+    def formula(person, period, parameters):
+        p = parameters(
+            period
+        ).gov.provinces.nt.tax.income.credits.pension_credit
+        federal_pension = person("pension_and_savings_plan_income", period)
+        return min_(p.max_amount, federal_pension)
