c5f51d5edd8918cfa2d77bc98afdb3bd342ad673
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-06-29 00:27:16 +0000

    Saskatchewan Pension Income Amount Fixes #357

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/pension_income_amount/sk_pension_income_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/pension_income_amount/sk_pension_income_credit.py
@@ -10,7 +10,9 @@ class sk_pension_income_credit(Variable):
     defined_for = ProvinceCode.SK
 
     def formula(person, period, parameters):
-        p = parameters(period).gov.provinces.sk.tax.income.credits.pension_income_amount
+        p = parameters(
+            period
+        ).gov.provinces.sk.tax.income.credits.pension_income_amount
         pension_payments = person("pension_and_savings_plan_income", period)
         max_amount = p.maximum_amount
         return min_(pension_payments, max_amount)
