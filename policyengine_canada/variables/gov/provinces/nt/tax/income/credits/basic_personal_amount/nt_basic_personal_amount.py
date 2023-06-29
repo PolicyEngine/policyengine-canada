f0ed1553b73f75233ad9463f4542cd00d2737693
Ziyu Wang <70183694+zqw5219@users.noreply.github.com>
2023-06-29 21:42:21 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_canada/variables/gov/provinces/nt/tax/income/credits/basic_personal_amount/nt_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/credits/basic_personal_amount/nt_basic_personal_amount.py
@@ -1,5 +1,6 @@
 from policyengine_canada.model_api import *
 
+
 class nt_basic_personal_amount(Variable):
     value_type = float
     entity = Person
@@ -7,8 +8,6 @@ class nt_basic_personal_amount(Variable):
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.NT
-    reference = "chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1nt/td1nt-23e.pdf"
+    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1nt/td1nt-23e.pdf"
 
-    def formula(person, period, parameters):
-        p = parameters(period).gov.provinces.nt.tax.income.credits.basic_personal_amount
-        return p.base
+    adds = "gov.provinces.nt.tax.income.credits.basic_personal_amount.base"
