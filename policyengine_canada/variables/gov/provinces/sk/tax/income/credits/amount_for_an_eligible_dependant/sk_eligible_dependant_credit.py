9396c3f6780a0db357641ebba154a59d3bbdcba0
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-06-30 14:00:34 +0000

    Fixes #362

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/amount_for_an_eligible_dependant/sk_eligible_dependant_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/amount_for_an_eligible_dependant/sk_eligible_dependant_credit.py
@@ -9,17 +9,21 @@ class sk_eligible_dependant_credit(Variable):
     defined_for = ProvinceCode.SK
 
     def formula(person, period, parameters):
-        p = parameters(period).gov.provinces.sk.tax.income.credits.amount_for_an_eligible_dependant
+        p = parameters(
+            period
+        ).gov.provinces.sk.tax.income.credits.amount_for_an_eligible_dependant
 
         dependant_income = person("dependant_income", period)
         live_with_dependant = person("joint_living", period)
         eligible = live_with_spouse
         reduction = where(
-            dependant_income_income <= p.net_income_base_amount, 0, dependant_income
+            dependant_income_income <= p.net_income_base_amount,
+            0,
+            dependant_income,
         )
         reduced_amount = p.maximum_amount - reduction
         return eligible * max_(0, reduced_amount)
-        
+
         credit = (
             person("mb_head_eligibility", period)
             * person("mb_dependant_eligibility", period)
--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit.py
@@ -13,7 +13,9 @@ class sk_spouse_or_common_law_partner_credit(Variable):
             period
         ).gov.provinces.sk.tax.income.credits.spouse_or_common_law_partner_amount
         spouse_income = person("spouse_income", period)
-        live_with_spouse = person("joint_living", period) & person("is_spouse", period)
+        live_with_spouse = person("joint_living", period) & person(
+            "is_spouse", period
+        )
         eligible = live_with_spouse
         reduction = where(
             spouse_income <= p.net_income_base_amount, 0, spouse_income
