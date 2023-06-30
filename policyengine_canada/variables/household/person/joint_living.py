d68b80661b1fcd402986888df09d6951c1ede1c4
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-06-30 13:48:28 +0000

    changed variables

--- policyengine_canada/variables/household/person/joint_living.py
+++ policyengine_canada/variables/household/person/joint_living.py
@@ -4,9 +4,6 @@ from policyengine_canada.model_api import *
 class joint_living(Variable):
     value_type = bool
     entity = Person
-    label = "Live with Spouse"
+    label = "Live together with the tax filer"
     unit = CAD
-    documentation = (
-        "A person who lives together with spouse or common-law partner"
-    )
     definition_period = YEAR
