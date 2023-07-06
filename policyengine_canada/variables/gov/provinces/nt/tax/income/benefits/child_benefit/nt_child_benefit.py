52024eff028158490ff30b70ed46d108b2eff15a
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-06 00:48:24 -0400

    make format

--- policyengine_canada/variables/gov/provinces/nt/tax/income/benefits/child_benefit/nt_child_benefit.py
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/benefits/child_benefit/nt_child_benefit.py
@@ -19,14 +19,14 @@ class nt_child_benefit(Variable):
         higher_threshold = p.income_threshold.higher
         income = household("adjusted_family_net_income", period)
         full_benefit_amount = younger_amount + older_amount
-        # Houshold net income is required to be below #30,000 to be 
+        # Houshold net income is required to be below #30,000 to be
         # eligible for the full benefit amount.
         full_benefit_eligibility = income <= lower_threshold
-        # Houshold net income is required to be below #80,000 to be 
+        # Houshold net income is required to be below #80,000 to be
         # eligible for the partial benefit amount.
         higher_income_eligibility = income < higher_threshold
-        # The full benefit is reduced by the division of the multiplication of the 
-        # full benefit amount as well as the difference between the income and the 
+        # The full benefit is reduced by the division of the multiplication of the
+        # full benefit amount as well as the difference between the income and the
         # difference between $80,000 and $30,000.
         partial_benefit_amount = full_benefit_amount - (
             ((income - lower_threshold) * full_benefit_amount)
