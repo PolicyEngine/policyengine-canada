c0610536a5464f14e91d87fb7dd2ee2daaf1fb3a
Ziyu Wang <70183694+zqw5219@users.noreply.github.com>
2023-07-11 22:31:57 +0000

    Fixes #380

--- policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount.py
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount.py
@@ -1,6 +1,5 @@
 from policyengine_canada.model_api import *
 
-
 class nt_disability_amount(Variable):
     value_type = float
     entity = Person
@@ -10,4 +9,10 @@ class nt_disability_amount(Variable):
     defined_for = ProvinceCode.NT
     reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1nt/td1nt-23e.pdf"
 
-    adds = "gov.provinces.nt.tax.income.credits.disability_amount.base"
+    def formula(person, period, parameters):
+        p = parameters(period).gov.provinces.nt.tax.income.credits.disability_amount
+        age = person("age", period)
+        dependant = person("is_dependant", period) # is this a bool?
+        total_expenses = person ("nt_childcare_expense", period) #?
+        eligible = age < p.age_eligibility  # this is a bool
+        return max_((eligible * ((p.max_amount - dependant * (total_expense - p.child_care_expenses)) + p.base)), p.base)
