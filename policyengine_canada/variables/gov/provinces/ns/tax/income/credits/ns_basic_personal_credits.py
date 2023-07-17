c9a2626ca11e41f8dde4ff464d67650edba2beca
okeyiii <96802936+okeyiii@users.noreply.github.com>
2023-07-17 22:03:07 +0000

    Complete

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_basic_personal_credits.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_basic_personal_credits.py
@@ -9,13 +9,25 @@ class ns_basic_personal_credits(Variable):
     defined_for = ProvinceCode.NS
 
     def formula(person, period, parameters):
-        p = parameters(period).gov.provinces.ns.tax.income.credits.ns_basic_personal_amount
-        taxable_income = person("total_individual_pre_tax_income",period)
+        p = parameters(
+            period
+        ).gov.provinces.ns.tax.income.credits.ns_basic_personal_amount
+        taxable_income = person("total_individual_pre_tax_income", period)
 
-        return select([taxable_income <= p.lower_income_threshold, 
-                p.lower_income_threshold < taxable_income < p.higher_income_threshold,
-                taxable_income >= p.higher_income_threshold],
-
-               [p.additional_amount + p.basic_amount,
-                p.basic_amount + 3000 - p.applicable_rate*(taxable_income - p.lower_income_threshold),
-                p.basic_amount])
+        return select(
+            [
+                taxable_income <= p.lower_income_threshold,
+                p.lower_income_threshold
+                < taxable_income
+                < p.higher_income_threshold,
+                taxable_income >= p.higher_income_threshold,
+            ],
+            [
+                p.additional_amount + p.basic_amount,
+                p.basic_amount
+                + 3000
+                - p.applicable_rate
+                * (taxable_income - p.lower_income_threshold),
+                p.basic_amount,
+            ],
+        )
