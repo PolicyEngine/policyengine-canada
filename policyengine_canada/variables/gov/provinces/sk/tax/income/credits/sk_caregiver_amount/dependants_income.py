adee6c2e7e893ce5748e9b1dd1a2fa51ae14e86b
Yaohhhh <128541260+Yaohhhh@users.noreply.github.com>
2023-07-19 19:29:45 +0000

    Pass Final Tests

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/sk_caregiver_amount/dependants_income.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/sk_caregiver_amount/dependants_income.py
@@ -8,5 +8,5 @@ class dependants_income(Variable):
     documentation = "The income of your spouse's or common-law partner's parent or grandparent or an infirm relative."
     unit = CAD
     definition_period = YEAR
-    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf"
+    reference = reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf""file:///C:/Users/yaoke/OneDrive/Desktop/PolicyEngine/SK/SK%20Tax%20Credit%20Return/td1sk-ws-23e%20(SK%20Tax%20Credit%20Calculation).pdf""file:///C:/Users/yaoke/OneDrive/Desktop/PolicyEngine/SK/SK%20Tax%20Credit%20Return/I2-01.pdf"
     defined_for = ProvinceCode.SK
--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/sk_caregiver_amount/sk_caregiver_amount.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/sk_caregiver_amount/sk_caregiver_amount.py
@@ -7,7 +7,7 @@ class sk_caregiver_amount(Variable):
     label = "Saskatchewan Caregiver Amount"
     unit = CAD
     definition_period = YEAR
-    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf"
+   reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf""file:///C:/Users/yaoke/OneDrive/Desktop/PolicyEngine/SK/SK%20Tax%20Credit%20Return/td1sk-ws-23e%20(SK%20Tax%20Credit%20Calculation).pdf""file:///C:/Users/yaoke/OneDrive/Desktop/PolicyEngine/SK/SK%20Tax%20Credit%20Return/I2-01.pdf"
     defined_for = ProvinceCode.SK
 
     def formula(person, period, parameters):
@@ -19,14 +19,14 @@ class sk_caregiver_amount(Variable):
     
         p = parameters(period).gov.provinces.sk.tax.income.credits.sk_caregiver_amount
 
-        age_threshold = select([is_elderly_dependant == 1 & is_infirm_dependant == 0,
-        is_elderly_dependant == 0 & is_infirm_dependant == 1,], 
+        age_threshold = select([(is_elderly_dependant == 1) & (is_infirm_dependant == 0),
+        (is_elderly_dependant == 0) & (is_infirm_dependant == 1),], 
         [p.elderly_age_threshold, p.infirm_age_threshold])
         age_eligibility = where(dependant_age >= age_threshold, 1, 0)
         
         eligibility = relative_live_eligibility & age_eligibility
 
-        amount = select([eligibility == 0 | dependants_income >= p.higher_income_threshold, eligibility == 1 & dependants_income <= p.lower_income_threshold, eligibility == 1 & p.higher_income_threshold > dependants_income > p.lower_income_threshold],
-        [0, p.amount, p.higher_income_threshold-dependants_income])
-
-        return amount
+        return select([eligibility == 0, (eligibility == 1) & (dependants_income >= p.higher_income_threshold), 
+        (eligibility == 1) & (dependants_income <= p.lower_income_threshold),
+        (eligibility == 1) & (p.higher_income_threshold > dependants_income > p.lower_income_threshold)],
+        [0, 0, p.amount, p.higher_income_threshold-dependants_income])
