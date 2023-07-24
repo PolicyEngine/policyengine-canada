cafa9ed2f3fe6dd8f2af8b2e196834a0e6287868
okeyiii <96802936+okeyiii@users.noreply.github.com>
2023-07-24 16:46:15 +0000

    Nova Scotia Basic Personal Amount Fixes #395

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_basic_personal_credits.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_basic_personal_credits.py
@@ -13,21 +13,30 @@ class ns_basic_personal_credits(Variable):
             period
         ).gov.provinces.ns.tax.income.credits.ns_basic_personal_amount
         taxable_income = person("total_individual_pre_tax_income", period)
+        
+        # Income Condition
+        lower_income_condition = (taxable_income <= p.lower_income_threshold)
+        medium_income_condition = (p.lower_income_threshold
+                                    < taxable_income
+                                    < p.higher_income_threshold)
+        higher_income_condition = (taxable_income >= p.higher_income_threshold)
+
+        # Basic Personal Amount
+        lower_basic_personal_amount = p.additional_amount + p.basic_amount
+        medium_basic_personal_amount = p.basic_amount + 3000 - p.applicable_rate * (taxable_income - p.lower_income_threshold)
+        higher_basic_personal_amount = p.basic_amount
+
+
 
         return select(
             [
-                taxable_income <= p.lower_income_threshold,
-                p.lower_income_threshold
-                < taxable_income
-                < p.higher_income_threshold,
-                taxable_income >= p.higher_income_threshold,
+                lower_income_condition,
+                medium_income_condition,
+                higher_income_condition,
             ],
             [
-                p.additional_amount + p.basic_amount,
-                p.basic_amount
-                + 3000
-                - p.applicable_rate
-                * (taxable_income - p.lower_income_threshold),
-                p.basic_amount,
+                lower_basic_personal_amount,
+                medium_basic_personal_amount,
+                higher_basic_personal_amount,
             ],
         )
