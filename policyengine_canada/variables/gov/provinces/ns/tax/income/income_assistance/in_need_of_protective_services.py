6ec67dcf079891d4d371b17f2b3df55b98f311cf
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-07-27 20:19:30 +0000

    Minor changes to resolve conversation

--- policyengine_canada/variables/gov/provinces/ns/tax/income/income_assistance/in_need_of_protective_services.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/income_assistance/in_need_of_protective_services.py
@@ -4,7 +4,7 @@ from policyengine_canada.model_api import *
 class in_need_of_protective_services(Variable):
     value_type = bool
     entity = Person
-    label = " In need of protective services"
+    label = "Person is in need of protective services"
     definition_period = YEAR
     reference = (
         "https://novascotia.ca/just/regulations/regs/esiaregs.htm#TOC3_8"
