38a036618a59a2514684b1a981a8da8279c38a93
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-07-31 19:15:01 +0000

    Quebec Basic Personal Amount Fixes #396

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/qc_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/qc_basic_personal_amount.py
@@ -8,9 +8,11 @@ class qc_basic_personal_amount(Variable):
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.QC
-    reference = "https://www.revenuquebec.ca/documents/en/formulaires/tp/2022-12/TP-1.D-V%282022-12%29.pdf"
+    reference = (
+        "https://www.revenuquebec.ca/documents/en/formulaires/tp/2022-12/TP-1.D-V%282022-12%29.pdf",
+        "https://www.revenuquebec.ca/en/citizens/income-tax-return/completing-your-income-tax-return/how-to-complete-your-income-tax-return/line-by-line-help/350-to-398-1-non-refundable-tax-credits/line-350/",
+    )
 
     def formula(household, period, parameters):
         p = parameters(period).gov.provinces.qc.tax.income.credits
-
         return p.basic_personal_amount
