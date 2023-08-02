cf6b554996cb6085fa4db9cbbfe187650fecda4d
sqw0418 <97478242+sqw0418@users.noreply.github.com>
2023-08-02 23:01:04 +0000

    Canada Employment Credit

--- policyengine_canada/variables/gov/cra/tax/income/credits/canada_employment_amount.py
+++ policyengine_canada/variables/gov/cra/tax/income/credits/canada_employment_amount.py
@@ -1,20 +1,21 @@
 from policyengine_canada.model_api import *
 
 
-class ca_employment_amount(Variable):
+class canada_employment_amount(Variable):
     value_type = float
     entity = Person
     label = "Canada Employment Amount"
     unit = CAD
     definition_period = YEAR
-    # defined_for = ProvinceCode.YT
     reference = "https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/deductions-credits-expenses/line-31260-canada-employment-amount.html"
 
     def formula(person, period, parameters):
-        maximun_return_amount = parameters(
+        max_amount = parameters(
             period
         ).gov.cra.tax.income.credits.canada_employment_amount.max_amount
 
         employment_income = person("employment_income", period)
+        other_employment_income = person("other_employment_income", period)
+        total_income = employment_income + other_employment_income
 
-        return min_(employment_income, maximun_return_amount)
+        return min_(total_income, max_amount)
