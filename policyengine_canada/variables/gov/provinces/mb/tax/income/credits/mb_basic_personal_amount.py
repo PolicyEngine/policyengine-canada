88b047ece686a36ca67cabb3295345a2c1779693
Yiwei Jia <yiwei.jia.career@gmail.com>
2023-08-06 16:28:25 -0400

    Add page number after reference

--- policyengine_canada/variables/gov/provinces/mb/tax/income/credits/mb_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/mb/tax/income/credits/mb_basic_personal_amount.py
@@ -7,7 +7,7 @@ class mb_basic_personal_amount(Variable):
     label = "Manitoba basic personal amount"
     unit = CAD
     definition_period = YEAR
-    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1mb/td1mb-23e.pdf"
+    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1mb/td1mb-23e.pdf" #page=1
     defined_for = ProvinceCode.MB
 
     def formula(person, period, parameters):
