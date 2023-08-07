e74d450babd77d09492da5980bf4ac239ee8d586
Yiwei Jia <yiwei.jia.career@gmail.com>
2023-08-07 17:47:56 -0400

    Update reference format and remove is_head

--- policyengine_canada/variables/gov/provinces/mb/tax/income/credits/mb_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/mb/tax/income/credits/mb_basic_personal_amount.py
@@ -7,10 +7,9 @@ class mb_basic_personal_amount(Variable):
     label = "Manitoba basic personal amount"
     unit = CAD
     definition_period = YEAR
-    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1mb/td1mb-23e.pdf"  # page=1
+    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1mb/td1mb-23e.pdf#page=1"
     defined_for = ProvinceCode.MB
 
     def formula(person, period, parameters):
-        is_head = person("is_head", period)
         p = parameters(period).gov.provinces.mb.credits
-        return is_head * p.basic_personal_amount
+        return p.basic_personal_amount
