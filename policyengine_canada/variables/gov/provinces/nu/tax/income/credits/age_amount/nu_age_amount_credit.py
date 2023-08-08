be130df5451a81d1921182d1a95cfb90a75fa172
runyao-yin <118035310+runyao-yin@users.noreply.github.com>
2023-08-08 15:17:48 -0400

    Update policyengine_canada/variables/gov/provinces/nu/tax/income/credits/age_amount/nu_age_amount_credit.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_canada/variables/gov/provinces/nu/tax/income/credits/age_amount/nu_age_amount_credit.py
+++ policyengine_canada/variables/gov/provinces/nu/tax/income/credits/age_amount/nu_age_amount_credit.py
@@ -7,7 +7,7 @@ class nu_age_amount_credit(Variable):
     label = "Nunvaut age amount credit"
     unit = CAD
     definition_period = YEAR
-    defined_for = ProvinceCode.NU
+    defined_for = "nu_age_amount_credit_eligible_person"
 
     def formula(person, period, parameters):
         income = person("total_individual_pre_tax_income", period)
