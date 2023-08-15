c526ad911ad242f0722c75904490539c94cab16e
okeyiii <96802936+okeyiii@users.noreply.github.com>
2023-08-15 03:47:23 +0000

    Nova Scotia Basic Personal Amount Fixes #395

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_basic_personal_amount.py
@@ -9,7 +9,7 @@ class ns_basic_personal_amount(Variable):
     defined_for = ProvinceCode.NS
     reference = (
         "https://hr.acadiau.ca/files/sites/hr/Payroll/Pensions%20&%20Benefits/NS_TD1_2022.pdf#page=1",
-        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/5003-d/5003-d-22e.pdf",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/5003-d/5003-d-22e.pdf#page=1",
         "https://nslegislature.ca/sites/default/files/legc/statutes/income%20tax.pdf#page=24",
     )
 
@@ -20,9 +20,7 @@ class ns_basic_personal_amount(Variable):
         taxable_income = person("total_individual_pre_tax_income", period)
 
         # Calculate additional amount added to base amount
-        exceedance = (
-            taxable_income - p.additional_amount.lower_income_threshold
-        )
+        exceedance = taxable_income - p.additional_amount.income_threshold
         reduced_additional_amount = (
             p.additional_amount.additional_amount
             - p.additional_amount.applicable_rate * exceedance
