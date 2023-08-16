56ef2bb599d813cd749c6faec9e80776d124a4ae
sqw0418 <97478242+sqw0418@users.noreply.github.com>
2023-08-16 22:47:33 +0000

    modify

--- policyengine_canada/variables/gov/provinces/yt/tax/income/benefits/yukon_employment_amount/yt_employment_amount.py
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/benefits/yukon_employment_amount/yt_employment_amount.py
@@ -11,10 +11,8 @@ class yt_employment_amount(Variable):
     reference = "https://www.canada.ca/en/revenue-agency/services/forms-publications/tax-packages-years/general-income-tax-benefit-package/yukon/5011-pc/information-residents-yukon.html#P4_58310"
 
     def formula(person, period, parameters):
-        max_amount = parameters(
+        p = parameters(
             period
-        ).gov.provinces.yt.benefits.yt_employment_amount.yt_employment_amount
-
-        employment_income = person("employment_income", period)
-
-        return min_(employment_income, max_amount)
+        ).gov.provinces.yt.ta.income.credits.employment_amount
+        canada_employment_amount = person("cananda_employment_amount", period)
+        return min_(canada_employment_amount, p.amount)
