3ff6f56e3f58a7d145b8d7a4fc050c5fcb31983a
Ziyu <91107507+ziyuliuzilla@users.noreply.github.com>
2023-08-21 17:08:11 +0000

    Minor changes

--- policyengine_canada/variables/household/income/household/property_value.py
+++ policyengine_canada/variables/household/income/household/property_value.py
@@ -5,6 +5,6 @@ class property_value(Variable):
     value_type = float
     entity = Household
     label = "Property value"
-    documentation = "Total equity in any real or personal property."
+    documentation = "Total equity in real property (land and attached assets) and personal property (tangible and intangible possessions apart from real estate)."
     unit = CAD
     definition_period = YEAR
