e2629ab0806e28579566bb297039313a506724eb
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-08-21 08:58:12 +0000

    Resolve part of comments.

---
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit_eligible.py
@@ -0,0 +1,16 @@
+from policyengine_canada.model_api import *
+
+
+class sk_spouse_or_common_law_partner_credit_eligible(Variable):
+    value_type = float
+    entity = Household
+    label = "Eligible for the Saskatchewan spouse or common law partner credit"
+    definition_period = YEAR
+    defined_for = ProvinceCode.SK
+
+    def formula(household, period, parameters):
+        live_with_spouse = household("cohabitating_spouses", period)
+        person = household.members
+        head = person("is_head", period)
+
+        return household.any(head) * live_with_spouse
