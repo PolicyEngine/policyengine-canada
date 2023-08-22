bcc76788d1629cbfc5d9075e3e191c799d525891
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-08-22 00:47:35 +0000

    Resolved comments.

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit.py
@@ -6,6 +6,13 @@ class sk_spouse_or_common_law_partner_credit(Variable):
     entity = Household
     label = "Saskatchewan spouse or common law partner credit"
     definition_period = YEAR
+    reference = (
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-22e.pdf#page=1",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf#page=1",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk-ws/td1sk-ws-22e.pdf#page=1",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk-ws/td1sk-ws-23e.pdf#page=1",
+        "https://publications.saskatchewan.ca/api/v1/products/583/formats/806/download#page=12",
+    )
     defined_for = "sk_spouse_or_common_law_partner_credit_eligible"
 
     def formula(household, period, parameters):
@@ -14,13 +21,7 @@ class sk_spouse_or_common_law_partner_credit(Variable):
         ).gov.provinces.sk.tax.income.credits.spouse_or_common_law_partner_amount
         person = household.members
         spouse_income = household.sum(person("spouse_income", period))
-        live_with_spouse = household("cohabitating_spouses", period)
-        reduction = where(
-            spouse_income <= p.reduction.income_threshold,
-            p.reduction.income_threshold,
-            spouse_income,
-        )
-        reduced_amount = max_(p.base_amount - reduction, 0)
-        head = person("is_head", period)
+        reduction_threshold = spouse_income <= p.reduction.income_threshold
+        reduced_amount = max_(p.reduction.base_amount - spouse_income, 0)
 
-        return household.any(head) * live_with_spouse * reduced_amount
+        return where(reduction_threshold, p.max_amount, reduced_amount)
