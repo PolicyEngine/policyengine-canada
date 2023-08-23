eb2fe4ed7a6b4bafe408fc77faa0deefd187311d
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-08-23 01:00:21 +0000

    created separate eligibility file

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/medical_expenses/qc_medical_expense_credit.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/medical_expenses/qc_medical_expense_credit.py
@@ -6,30 +6,8 @@ class qc_medical_expense_credit(Variable):
     entity = Person
     label = "Quebec medical expenses tax credit"
     definition_period = YEAR
-    defined_for = ProvinceCode.QC
+    defined_for = "qc_medical_expense_credit_eligible"
 
     def formula(person, period, parameters):
-        p = parameters(
-            period
-        ).gov.provinces.qc.tax.income.credits.medical_expenses
-
         medical_expenses = person("medical_expenses", period)
-
-        # age eligibility
-        age_eligible = person("age", period) >= p.age_eligibility
-
-        # work income eligibility
-        work_income_eligible = (
-            person("working_income", period) >= p.work_income_base
-        )
-
-        # compare family income with maximum family income
-        income = person("individual_net_income", period)
-        spouse_income = person("spouse_income", period)
-        family_income_eligible = (
-            income + spouse_income
-        ) < p.maximum_family_income.calc(medical_expenses)
-
-        eligible = age_eligible * work_income_eligible * family_income_eligible
-
-        return eligible * medical_expenses
+        return medical_expenses
