b0cf36e8a26a07e2373708595c05d18ce6c6a5d8
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-28 23:08:17 +0300

    partial fixes

--- policyengine_canada/variables/gov/provinces/yt/tax/income/credits/yt_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/credits/yt_basic_personal_amount.py
@@ -10,10 +10,7 @@ class yt_basic_personal_amount(Variable):
 
     def formula(person, period, parameters):
         income = person("individual_net_income", period)
-        head = person("is_head", period)
         p = parameters(
             period
         ).gov.provinces.yt.tax.income.credits.basic_personal_amount
-        reduction = p.reduction_rate.calc(income)
-        base = p.basic_personal_amount
-        return (base + reduction) * head
+        return p.amount.calc(income)
