2dbeacf562a77f150c55875cd6de1b33b3389480
Ziyu Wang <70183694+zqw5219@users.noreply.github.com>
2023-08-29 23:22:46 +0000

    fixes #380

--- policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount_self.py
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount_self.py
@@ -1,22 +1,26 @@
 from policyengine_canada.model_api import *
 
+
 class nt_disability_amount_self(Variable):
     value_type = float
     entity = Person
     label = "Northwest Territories Disability Amount for self"
     unit = CAD
     definition_period = YEAR
-    defined_for = ProvinceCode.NT
+    defined_for = "nt_disability_amount_self_eligible"
     reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1nt/td1nt-23e.pdf"
 
     def formula(person, period, parameters):
         household = person.household
-        p = parameters(period).gov.provinces.nt.tax.income.credits.disability_amount_self
+        p = parameters(
+            period
+        ).gov.provinces.nt.tax.income.credits.disability_amount_self
         age_eligible = person("age", period) < p.age_eligibility
-        head = person("is_head", period)
-        disability = person("is_disabled", period)
-        eligible = head & disability
         total_expenses = household("childcare_costs", period)
         expense_threshold = max_(total_expenses - p.child_care_expense_cap, 0)
-        additional_max_with_child_care = max_(p.max_additional_amount - expense_threshold, 0)
-        return eligible * where(age_eligible, p.base + additional_max_with_child_care, p.base)
+        additional_max_with_child_care = max_(
+            p.max_additional_amount - expense_threshold, 0
+        )
+        return where(
+            age_eligible, p.base + additional_max_with_child_care, p.base
+        )
--- policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount_self_eligible.py
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount_self_eligible.py
@@ -1,18 +1,13 @@
 from policyengine_canada.model_api import *
 
+
 class nt_disability_amount_self_eligible(Variable):
-    value_type = float
+    value_type = bool
     entity = Person
     label = "Northwest Territories Disability Amount for self elligibility"
-    unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.NT
     reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1nt/td1nt-23e.pdf"
 
     def formula(person, period, parameters):
-        p = parameters(period).gov.provinces.nt.tax.income.credits.disability_amount_self
-        age_eligible = person("age", period) < p.age_eligibility
-        head = person("is_head", period)
-        disability = person("is_disabled", period)
-        eligible = head & disability
-        return age_eligible * eligible
+        return person("is_disabled", period)
