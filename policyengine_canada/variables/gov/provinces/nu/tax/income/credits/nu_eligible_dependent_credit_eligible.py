cf29afdabad6d1a171e15d22069ee9ecbc5b132f
runyao-yin <118035310+runyao-yin@users.noreply.github.com>
2023-08-29 20:35:18 -0400

    Update nu_eligible_dependent_credit_eligible.py

--- policyengine_canada/variables/gov/provinces/nu/tax/income/credits/nu_eligible_dependent_credit_eligible.py
+++ policyengine_canada/variables/gov/provinces/nu/tax/income/credits/nu_eligible_dependent_credit_eligible.py
@@ -4,7 +4,7 @@ from policyengine_canada.model_api import *
 class nu_eligible_dependent_credit_eligible(Variable):
     value_type = float
     entity = Person
-    label = "Nunavut eligible dependent credit"
+    label = "Nunavut eligible dependent credit eligible"
     definition_period = YEAR
     defined_for = ProvinceCode.NU
 
