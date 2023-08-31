cbc0f9f3b058c31d468db38bf6bf343bc32cb3c0
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-08-31 06:28:13 +0000

    Change entity.

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit.py
@@ -3,7 +3,7 @@ from policyengine_canada.model_api import *
 
 class sk_spouse_or_common_law_partner_credit(Variable):
     value_type = float
-    entity = Household
+    entity = Person
     label = "Saskatchewan spouse or common law partner credit"
     definition_period = YEAR
     reference = (
@@ -15,12 +15,11 @@ class sk_spouse_or_common_law_partner_credit(Variable):
     )
     defined_for = "sk_spouse_or_common_law_partner_credit_eligible"
 
-    def formula(household, period, parameters):
+    def formula(person, period, parameters):
         p = parameters(
             period
         ).gov.provinces.sk.tax.income.credits.spouse_or_common_law_partner_amount
-        person = household.members
-        spouse_income = household.sum(person("spouse_income", period))
+        spouse_income = person("spouse_income", period)
         reduction_threshold = spouse_income <= p.reduction.income_threshold
         reduced_amount = max_(p.reduction.base_amount - spouse_income, 0)
 
--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit_eligible.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/spouse_or_common_law_partner_amount/sk_spouse_or_common_law_partner_credit_eligible.py
@@ -14,8 +14,4 @@ class sk_spouse_or_common_law_partner_credit_eligible(Variable):
     defined_for = ProvinceCode.SK
 
     def formula(household, period, parameters):
-        live_with_spouse = household("cohabitating_spouses", period)
-        person = household.members
-        head = person("is_head", period)
-
-        return household.any(head) * live_with_spouse
+        return household("cohabitating_spouses", period)
