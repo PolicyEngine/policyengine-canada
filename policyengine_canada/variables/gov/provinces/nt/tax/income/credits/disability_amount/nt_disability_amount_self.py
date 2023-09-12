bb67df8c83cc437a2a86a9a5c310a941ad4839f6
Ziyu Wang <70183694+zqw5219@users.noreply.github.com>
2023-09-12 23:19:37 +0000

    fixes #380

--- policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount_self.py
+++ policyengine_canada/variables/gov/provinces/nt/tax/income/credits/disability_amount/nt_disability_amount_self.py
@@ -4,26 +4,25 @@ from policyengine_canada.model_api import *
 class nt_disability_amount_self(Variable):
     value_type = float
     entity = Person
-    label = "Northwest Territories Disability amount for self"
+    label = "Northwest Territories disability amount for self"
     unit = CAD
     definition_period = YEAR
     defined_for = "nt_disability_amount_self_eligible"
-    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1nt/td1nt-23e.pdf"
+    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/5012-d/5012-d-22e.pdf#page=2"
 
     def formula(person, period, parameters):
-        household = person.household
         p = parameters(
             period
         ).gov.provinces.nt.tax.income.credits.disability_amount_self
         reduction_age_threshold = (
             person("age", period) < p.additional_amount.age_eligibility
         )
-        total_expenses = household("childcare_costs", period)
-        expense_threshold = max_(
+        total_expenses = person("care_costs_for_self", period)
+        expense_reduction = max_(
             total_expenses - p.additional_amount.child_care_expense_cap, 0
         )
         additional_max_with_child_care = max_(
-            p.additional_amount.max_amount - expense_threshold, 0
+            p.additional_amount.max_amount - expense_reduction, 0
         )
         return where(
             reduction_age_threshold,
