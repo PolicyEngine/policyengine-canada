c77e1c30ce499a00d254d639bae925cbec024f19
laviniawo <132245041+laviniawo@users.noreply.github.com>
2023-09-13 04:11:46 +0000

    resolve comments

--- policyengine_canada/variables/gov/provinces/mb/tax/income/credits/spouse_and_common_law_partner_amount/mb_spouse_credit_amount.py
+++ policyengine_canada/variables/gov/provinces/mb/tax/income/credits/spouse_and_common_law_partner_amount/mb_spouse_credit_amount.py
@@ -4,19 +4,16 @@ from policyengine_canada.model_api import *
 class mb_spouse_credit_amount(Variable):
     value_type = float
     entity = Person
-    label = "Manitoba spouse and common law partner amount"
+    label = "Manitoba spouse and common-law partner amoount"
     definition_period = YEAR
     defined_for = "mb_spouse_credit_eligible"
 
     def formula(person, period, parameters):
-
         p = parameters(
             period
         ).gov.provinces.mb.tax.income.credits.spouse_or_common_law_partner_amount
 
         spouse_income = person("spouse_income", period)
-        living_together = person("cohabitating_spouses", period)
+        is_caregiver = person("is_caregiver", period)
 
-        credit_amount = max_(0, (p.base - spouse_income))
-
-        return living_together * credit_amount
+        return is_caregiver * (max_(0, (p.base - spouse_income)))
--- policyengine_canada/variables/gov/provinces/mb/tax/income/credits/spouse_and_common_law_partner_amount/mb_spouse_credit_eligible.py
+++ policyengine_canada/variables/gov/provinces/mb/tax/income/credits/spouse_and_common_law_partner_amount/mb_spouse_credit_eligible.py
@@ -3,10 +3,10 @@ from policyengine_canada.model_api import *
 
 class mb_spouse_credit_eligible(Variable):
     value_type = bool
-    entity = Person
+    entity = Household
     label = "Manitoba head eligiblility for recieving spouse's tax credit"
     definition_period = YEAR
     defined_for = ProvinceCode.MB
 
-    def formula(person, period, parameters):
-        return person("is_caregiver", period)
+    def formula(household, period, parameters):
+        return household("cohabitating_spouses", period)
