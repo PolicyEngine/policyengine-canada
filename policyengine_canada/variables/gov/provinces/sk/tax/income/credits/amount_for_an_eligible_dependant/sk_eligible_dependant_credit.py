d09b35f9be668c871552d77999938cb25b48beec
LanxueHe <103899111+Snownow0w@users.noreply.github.com>
2023-09-15 01:12:19 +0000

    Fixes #371

--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/amount_for_an_eligible_dependant/sk_eligible_dependant_credit.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/amount_for_an_eligible_dependant/sk_eligible_dependant_credit.py
@@ -11,7 +11,7 @@ class sk_eligible_dependant_credit(Variable):
         "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf#page=1",
         "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk-ws/td1sk-ws-22e.pdf#page=1",
         "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk-ws/td1sk-ws-23e.pdf#page=1",
-        "https://publications.saskatchewan.ca/api/v1/products/583/formats/806/download#page=16",
+        "https://publications.saskatchewan.ca/api/v1/products/583/formats/806/download#page=13",  # page=16
     )
     defined_for = "sk_eligible_dependant_credit_eligibility"
 
@@ -19,7 +19,6 @@ class sk_eligible_dependant_credit(Variable):
         p = parameters(
             period
         ).gov.provinces.sk.tax.income.credits.amount_for_an_eligible_dependant
-
         dependant_income = person("dependant_income", period)
         reduction_threshold = dependant_income <= p.reduction.income_threshold
         reduced_amount = max_(p.reduction.base_amount - dependant_income, 0)
--- policyengine_canada/variables/gov/provinces/sk/tax/income/credits/amount_for_an_eligible_dependant/sk_eligible_dependant_credit_eligibility.py
+++ policyengine_canada/variables/gov/provinces/sk/tax/income/credits/amount_for_an_eligible_dependant/sk_eligible_dependant_credit_eligibility.py
@@ -6,11 +6,17 @@ class sk_eligible_dependant_credit_eligibility(Variable):
     entity = Person
     label = "Eligibility for the Saskatchewan amount for an eligible dependant credit"
     definition_period = YEAR
+    reference = (
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-22e.pdf#page=1",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk/td1sk-23e.pdf#page=1",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk-ws/td1sk-ws-22e.pdf#page=1",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1sk-ws/td1sk-ws-23e.pdf#page=1",
+        "https://publications.saskatchewan.ca/api/v1/products/583/formats/806/download#page=13",  # page=16
+    )
     defined_for = ProvinceCode.SK
 
     def formula(person, period, parameters):
-        household = person.household
-        live_together = household("joint_living", period)
+        live_together = person.household("joint_living", period)
         dependant = person("is_dependant", period)
         is_related = person("is_relative", period)
         dependant_eligible = live_together & dependant & is_related
