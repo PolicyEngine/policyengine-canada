9adfa9dead9d5e355b78a02bda7ca1b5cdc46e28
okeyiii <96802936+okeyiii@users.noreply.github.com>
2023-09-15 03:52:15 +0000

    Nova Scotia Disability amount Fixes #442 Added param, var and test

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_disability_amount.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/ns_disability_amount.py
@@ -1,2 +1,19 @@
-# peronal 
-# boolean diablitiy input
+from policyengine_canada.model_api import *
+
+
+class ns_disability_amount(Variable):
+    value_type = bool
+    entity = Person
+    label = "Nova Scotia Disability Amount"
+    definition_period = YEAR
+    defined_for = ProvinceCode.NS
+    reference = "https://hr.acadiau.ca/files/sites/hr/Payroll/Pensions%20&%20Benefits/NS_TD1_2022.pdf#page=1"
+
+    def formula(person, period, parameters):
+        p = parameters(
+            period
+        ).gov.provinces.ns.tax.income.credits.disability_amount
+        isdisabled = person("person_disability_situation", period)
+        disability_amount = p.amount
+
+        return where(isdisabled, disability_amount, 0)
