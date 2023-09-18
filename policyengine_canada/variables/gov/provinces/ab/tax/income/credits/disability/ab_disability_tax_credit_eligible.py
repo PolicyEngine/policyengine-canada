2cde51bb3f2ce7ebc276c71043cc2841f387cd7c
7qiwx <49327053+7qiwx@users.noreply.github.com>
2023-09-18 21:16:09 +0000

    Fixes

--- policyengine_canada/variables/gov/provinces/ab/tax/income/credits/disability/ab_disability_tax_credit_eligible.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/income/credits/disability/ab_disability_tax_credit_eligible.py
@@ -4,7 +4,7 @@ from policyengine_canada.model_api import *
 class ab_disability_tax_credit_eligible(Variable):
     value_type = bool
     entity = Person
-    label = "Alberta disability tax credit eligbility"
+    label = "Eligible for the Alberta disability tax credit"
     unit = CAD
     definition_period = YEAR
     defined_for = ProvinceCode.AB
