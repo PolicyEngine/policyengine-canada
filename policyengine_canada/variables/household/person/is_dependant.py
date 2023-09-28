aeeedd9bb3fd3889833f182c9aa79b390e244fc3
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-09-28 14:24:48 +0000

    Fixes #446

--- policyengine_canada/variables/household/person/is_dependant.py
+++ policyengine_canada/variables/household/person/is_dependant.py
@@ -4,9 +4,10 @@ from policyengine_canada.model_api import *
 class is_dependant(Variable):
     value_type = bool
     entity = Person
-    label = "Is a dependant"
+    label = "Is a dependant in the household"
     definition_period = YEAR
 
-    # Impute dependant status on age.
     def formula(person, period, parameters):
-        return person("age", period) < 18
+        head = person("head", period)
+        spouse = person("spouse", period)
+        return ~head & ~spouse
