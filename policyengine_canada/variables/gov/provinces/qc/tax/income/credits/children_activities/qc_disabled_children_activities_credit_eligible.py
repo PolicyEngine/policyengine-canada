cb8bf02858af38344f864c927e5047fb415fb00d
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-10-04 18:58:53 +0000

    check is_dependant

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/children_activities/qc_disabled_children_activities_credit_eligible.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/children_activities/qc_disabled_children_activities_credit_eligible.py
@@ -17,8 +17,6 @@ class qc_disabled_children_activities_credit_eligible(Variable):
         disabled = person("is_disabled", period)
 
         # your or your spouse's child or a person of whom you or your spouse has the custody and supervision
-        is_child_of_filer = person("is_child_of_filer", period)
-        is_custodied_by_filer = person("is_custodied_by_filer", period)
-        eligible_child = is_child_of_filer | is_custodied_by_filer
+        is_dependant = person("is_dependant", period)
 
-        return eligible_child & disabled & p.age_eligibility.calc(age)
+        return is_dependant & disabled & p.age_eligibility.calc(age)
--- policyengine_canada/variables/household/person/is_custodied_by_filter.py
+++
@@ -1,8 +0,0 @@
-from policyengine_canada.model_api import *
-
-
-class is_custodied_by_filer(Variable):
-    value_type = bool
-    entity = Person
-    label = "Is custodied or supervised by the tax filer or their spouse"
-    definition_period = YEAR
