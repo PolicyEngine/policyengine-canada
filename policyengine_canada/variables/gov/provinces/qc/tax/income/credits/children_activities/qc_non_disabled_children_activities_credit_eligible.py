f0956ebf94d47ef0ac1c34712679339a26d78e29
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-10-04 19:04:01 +0000

    fix test errors

--- policyengine_canada/variables/gov/provinces/qc/tax/income/credits/children_activities/qc_non_disabled_children_activities_credit_eligible.py
+++ policyengine_canada/variables/gov/provinces/qc/tax/income/credits/children_activities/qc_non_disabled_children_activities_credit_eligible.py
@@ -16,10 +16,8 @@ class qc_non_disabled_children_activities_credit_eligible(Variable):
         ).gov.provinces.qc.tax.income.credits.children_activities.non_disabled_children
 
         # your or your spouse's child or a person of whom you or your spouse has the custody and supervision
-        is_child_of_filer = person("is_child_of_filer", period)
-        is_custodied_by_filer = person("is_custodied_by_filer", period)
-        eligible_child = is_child_of_filer | is_custodied_by_filer
+        is_dependant = person("is_dependant", period)
 
         age = person("age", period)
         non_disabled = ~person("is_disabled", period)
-        return eligible_child & non_disabled & p.age_eligibility.calc(age)
+        return is_dependant & non_disabled & p.age_eligibility.calc(age)
