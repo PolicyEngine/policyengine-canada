8414dded4de0d50292e3a1db2da8f5a35082f0a3
okeyiii <96802936+okeyiii@users.noreply.github.com>
2023-10-15 18:37:48 +0000

    Nova Scotia Age Amount Supplement Create pr and new files Fix #issue455

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/age/ns_age_amount_supplement.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/age/ns_age_amount_supplement.py
@@ -10,8 +10,8 @@ class ns_age_amount_supplement(Variable):
     defined_for = ProvinceCode.NS
     reference = (
         "https://hr.acadiau.ca/files/sites/hr/Payroll/Pensions%20&%20Benefits/NS_TD1_2022.pdf#page=1",
-        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/5003-d/5003-d-22e.pdf#page=1",
-        "https://nslegislature.ca/sites/default/files/legc/statutes/income%20tax.pdf#page=24",
+        "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1ns-ws/td1ns-ws-23e.pdf#page=1",
+        "https://nslegislature.ca/sites/default/files/legc/statutes/income%20tax.pdf#page=28",
     )
 
     def formula(person, period, parameters):
@@ -29,4 +29,4 @@ class ns_age_amount_supplement(Variable):
             - p.age_supplement.phase_out_rate.calc(taxable_income),
         )
 
-        return reduced_additional_amount
+        return eligibility * reduced_additional_amount
