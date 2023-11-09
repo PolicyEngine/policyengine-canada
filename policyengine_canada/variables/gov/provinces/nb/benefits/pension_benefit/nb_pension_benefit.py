e284dc5472df0b15cf843b29b3b52a766298220e
Yiwei Jia <yiwei.jia.career@gmail.com>
2023-11-09 15:41:39 -0500

    Renamed yaml file

--- policyengine_canada/variables/gov/provinces/nb/benefits/pension_benefit/nb_pension_benefit.py
+++ policyengine_canada/variables/gov/provinces/nb/benefits/pension_benefit/nb_pension_benefit.py
@@ -14,6 +14,6 @@ class nb_pension_benefit(Variable):
         pension_income = person("pension_and_savings_plan_income", period)
         max_amount = parameters(
             period
-        ).gov.provinces.nb.benefits.pension_benefit.max_amount
+        ).gov.provinces.nb.benefits.pension_benefit.cap
         # Capped at a certain amount
         return min_(max_amount, pension_income)
