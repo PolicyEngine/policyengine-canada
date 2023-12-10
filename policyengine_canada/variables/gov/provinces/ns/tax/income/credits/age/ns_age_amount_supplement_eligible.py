e73b3c0fcc0bf83792a3d8ac9f1c65494a51cc57
okeyiii <96802936+okeyiii@users.noreply.github.com>
2023-12-10 17:11:52 +0000

    Issue458
    Add ns_age_amount_supplement_eligible

--- policyengine_canada/variables/gov/provinces/ns/tax/income/credits/age/ns_age_amount_supplement_eligible.py
+++ policyengine_canada/variables/gov/provinces/ns/tax/income/credits/age/ns_age_amount_supplement_eligible.py
@@ -1,13 +1,13 @@
 from policyengine_canada.model_api import *
 
 
-class ns_age_amount_supplement(Variable):
+class ns_age_amount_supplement_eligible(Variable):
     value_type = float
     entity = Person
-    label = "Nova Scotia Age Amount Supplement"
+    label = "Nova Scotia Age Amount Supplement Eligible"
     unit = CAD
     definition_period = YEAR
-    defined_for = ns_age_amount_supplement_eligible
+    defined_for = ProvinceCode.NS
     reference = (
         "https://hr.acadiau.ca/files/sites/hr/Payroll/Pensions%20&%20Benefits/NS_TD1_2022.pdf#page=1",
         "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1ns-ws/td1ns-ws-23e.pdf#page=1",
@@ -16,11 +16,7 @@ class ns_age_amount_supplement(Variable):
 
     def formula(person, period, parameters):
         p = parameters(period).gov.provinces.ns.tax.income.credits.age
-        taxable_income = person("ns_taxable_income", period)
+        age = person("age", period)
 
-        # Calculate additional amount added to base amount
-        return max_(
-            0,
-            p.supplement.base
-            - p.supplement.phase_out_rate.calc(taxable_income),
-        )
+        # is eligible for age amount supplement
+        return age >= p.age_eligibility
