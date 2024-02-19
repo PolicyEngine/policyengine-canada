3934ef6d7957c7c144d1c9bfca4d5160fcf8fb08
7qiwx <49327053+7qiwx@users.noreply.github.com>
2024-02-19 04:10:55 +0000

    Fix label

--- policyengine_canada/variables/household/person/care_expenses.py
+++ policyengine_canada/variables/household/person/care_expenses.py
@@ -4,6 +4,6 @@ from policyengine_canada.model_api import *
 class care_expenses(Variable):
     value_type = float
     entity = Person
-    label = "Amount of childcare expenses incurred by the child"
+    label = "Amount of care expenses incurred by the person"
     unit = CAD
     definition_period = YEAR
