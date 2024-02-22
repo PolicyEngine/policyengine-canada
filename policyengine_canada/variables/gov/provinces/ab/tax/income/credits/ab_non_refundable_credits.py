3f75242c42d20546ff33dc76edf2b518aecb5747
7qiwx <49327053+7qiwx@users.noreply.github.com>
2024-02-22 22:31:42 +0000

    Add refundable and non_refundable credits python and yaml files

---
+++ policyengine_canada/variables/gov/provinces/ab/tax/income/credits/ab_non_refundable_credits.py
@@ -0,0 +1,11 @@
+from policyengine_canada.model_api import *
+
+
+class ab_non_refundable_credits(Variable):
+    value_type = float
+    entity = Person
+    label = "Alberta non-refundable credits"
+    unit = CAD
+    definition_period = YEAR
+    defined_for = ProvinceCode.AB
+    adds = "gov.provinces.ab.tax.income.credits.non_refundable"
