a9f7c3663f9cc2b77ce29499cdd097b87a195e7d
7qiwx <49327053+7qiwx@users.noreply.github.com>
2024-02-27 01:30:48 +0000

    British Columbia Disability Credit Fixes #464

--- policyengine_canada/variables/household/person/childcare_received.py
+++
@@ -1,9 +0,0 @@
-from policyengine_canada.model_api import *
-
-
-class childcare_received(Variable):
-    value_type = float
-    entity = Person
-    label = "Amount of Childcare received which was claimed by another person"
-    unit = CAD
-    definition_period = YEAR
