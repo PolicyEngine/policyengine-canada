c64ef495f16885c4c2f55a0f2c17035ebfa0d746
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-29 03:12:56 +0100

    format

--- policyengine_canada/variables/gov/provinces/bc/tax/income/credits/bc_disability_credit/bc_disability_credit.py
+++ policyengine_canada/variables/gov/provinces/bc/tax/income/credits/bc_disability_credit/bc_disability_credit.py
@@ -15,5 +15,4 @@ class bc_disability_credit(Variable):
         additional_amount = person(
             "bc_disability_credit_additional_amount", period
         )
-        uncapped_amount = p.base + additional_amount
-        return min_(p.cap, uncapped_amount)
+        return p.base + additional_amount
