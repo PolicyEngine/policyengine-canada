83f2e87fa43e9c93c861d3fcf6651f7d1d65804c
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-29 03:33:28 +0100

    format

--- policyengine_canada/variables/gov/provinces/bc/tax/income/credits/pension_income_amount/bc_pension_income_amount.py
+++ policyengine_canada/variables/gov/provinces/bc/tax/income/credits/pension_income_amount/bc_pension_income_amount.py
@@ -11,8 +11,8 @@ class bc_pension_income_amount(Variable):
 
     def formula(person, period, parameters):
         pension_income = person("pension_and_savings_plan_income", period)
-        max_amount = parameters(
+        p = parameters(
             period
-        ).gov.provinces.bc.tax.income.credits.pension_income_amount.cap
+        ).gov.provinces.bc.tax.income.credits.pension_income_amount
         # Capped at a certain amount
-        return min_(max_amount, pension_income)
+        return min_(p.cap, pension_income)
