3ee6e3c46e58d59e7c702b7af97daaf73dded361
7qiwx <49327053+7qiwx@users.noreply.github.com>
2024-03-14 21:26:47 +0000

    Fixed variable name

--- policyengine_canada/variables/household/income/household/household_benefits.py
+++ policyengine_canada/variables/household/income/household/household_benefits.py
@@ -8,8 +8,8 @@ class household_benefits(Variable):
     unit = CAD
     definition_period = YEAR
     adds = [
-        "Canada_child_benefit",
-        "Canada_dental_benfit",
+        "child_benefit",
+        "dental_benfit",
         "mb_child_benefit",
         "nb_child_benefit",
         "nu_child_benefit",
