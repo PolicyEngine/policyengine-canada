072170e730dd0c8aacf7639f0f9aa94fb2da5352
SirMalamute <neeliyer14@gmail.com>
2024-04-21 21:19:10 +0000

    Alberta base personal credit Fixes #376

--- policyengine_canada/variables/gov/provinces/ab/tax/income/credits/basic_personal_amount/ab_base_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/income/credits/basic_personal_amount/ab_base_personal_amount.py
@@ -1,15 +1,14 @@
 from policyengine_canada.model_api import *
 
 
-class ab_base_personal_credit(Variable):
+class ab_base_personal_amount(Variable):
     value_type = float
     entity = Person
-    label = "Alberta base personal credit"
+    label = "Alberta basic personal amount"
     unit = CAD
     definition_period = YEAR
     reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1ab/td1ab-23e.pdf"
     defined_for = ProvinceCode.AB
 
     def formula(person, period, parameters):
-        base = parameters(period).gov.provinces.ab.tax.income.credits.base
-        return base
+        return parameters(period).gov.provinces.ab.tax.income.credits.basic_personal_amount.amount
