2895c86c77aa8268cfbeeb8f7cb3b4990e8a1088
SirMalamute <neeliyer14@gmail.com>
2024-04-29 20:09:24 +0000

    spelling

--- policyengine_canada/tests/household/cohabitating_dependant.py
+++ policyengine_canada/tests/household/cohabitating_dependant.py
@@ -5,6 +5,6 @@ class cohabitating_dependant(Variable):
     value_type = bool
     entity = Person
     label = "Dependant living with the head of household"
-    documentation = "Whthere your spouse's or common-law partner's dependant lives with you or not."
+    documentation = "Whether your spouse's or common-law partner's dependant lives with you or not."
     definition_period = YEAR
     reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/td1ab/td1ab-23e.pdf#page=1"
