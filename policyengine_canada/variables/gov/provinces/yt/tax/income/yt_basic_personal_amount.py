203b6d1dc23161fee4ce61368ad9f17394041fd5
yy3187 <129118329+yy3187@users.noreply.github.com>
2024-05-13 15:30:09 +0000

    update variables

--- policyengine_canada/variables/gov/provinces/yt/tax/income/yt_basic_personal_amount.py
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/yt_basic_personal_amount.py
@@ -13,19 +13,14 @@ class yt_basic_personal_amount(Variable):
             period
         ).gov.provinces.yt.tax.income.credits.basic_personal_amount
 
-        applicable_amount = p.applicable_amount
-        base_amount = p.base_amount
-        divisor = p.divisor
-        income_threshold = p.income_threshold
-
         individual_net_income = person("individual_net_income", period)
-        additional_amount = divisor - (
-            individual_net_income - income_threshold
+        additional_amount = p.divisor - (
+            individual_net_income - p.income_threshold
         )
         additional_amount_eligible = additional_amount <= 0
 
         return where(
             additional_amount_eligible,
-            base_amount,
-            additional_amount / divisor * applicable_amount + base_amount,
+            p.base_amount,
+            additional_amount / p.divisor * p.applicable_amount + p.base_amount,
         )
