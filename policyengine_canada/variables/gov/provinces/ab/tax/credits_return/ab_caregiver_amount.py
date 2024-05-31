beca7f9a2a97de9b535e711a74b20351adce0974
SirMalamute <neeliyer14@gmail.com>
2024-05-31 03:44:09 +0000

    minor change

--- policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
@@ -19,7 +19,7 @@ class ab_caregiver_amount(Variable):
         age = person("age", period)
         disabled = person("is_disabled", period)
         is_dependant = (age >= p.age_eligibility.upper) | (
-            age >= p.age_eligibility.lower & person("is_disabled", period)
+            age >= p.age_eligibility.lower & disabled
         )
 
         income = person("individual_net_income", period)
