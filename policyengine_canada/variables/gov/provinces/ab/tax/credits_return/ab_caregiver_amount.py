f989cfccac2f3f486718b91e5224fea897201a1d
SirMalamute <neeliyer14@gmail.com>
2024-06-03 15:49:47 -0400

    Update policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
@@ -18,8 +18,9 @@ class ab_caregiver_amount(Variable):
         # Whether the person is dependant (65 or older/ disabled)
         age = person("age", period)
         disabled = person("is_disabled", period)
-        is_dependant = (age >= p.age_eligibility.upper) | (
-            age >= p.age_eligibility.lower & disabled
+        is_dependant = person("is_dependant", period)
+        eligible_dependant = ((age >= p.age_eligibility.upper) | (
+            age >= p.age_eligibility.lower & disabled) & is_dependant
         )
 
         income = person("individual_net_income", period)
