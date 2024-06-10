10458988639bd161894bc7a97c0538cebf3288eb
SirMalamute <neeliyer14@gmail.com>
2024-06-10 00:05:29 +0000

    format

--- policyengine_canada/variables/gov/provinces/ab/tax/income/credits/infirm_dependant/ab_infirm_dependant_credit.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/income/credits/infirm_dependant/ab_infirm_dependant_credit.py
@@ -22,10 +22,12 @@ class ab_infirm_dependant_credit(Variable):
 
         max_credit = eligible * p.base
 
-        reduced_threshold = max_(0, p.income_threshold - infirm_dependant_income)
+        reduced_threshold = max_(
+            0, p.income_threshold - infirm_dependant_income
+        )
 
         partial_credit = eligible * reduced_threshold
-        
+
         capped_credit = where(
             income_level_condition,
             partial_credit,
