53d6b7e9d6426f39c97f7abf2683bb5d718ea309
SirMalamute <neeliyer14@gmail.com>
2024-07-01 15:01:30 +0000

    minor

--- policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
+++ policyengine_canada/variables/gov/provinces/ab/tax/credits_return/ab_caregiver_amount.py
@@ -26,7 +26,7 @@ class ab_caregiver_amount(Variable):
         cohabitating_dependant = person("cohabitating_dependant", period)
 
         caregiver_amount = (
-            net_income * income_eligibility * cohabitating_dependant
+            net_income * person("ab_caregiver_amount_eligible", period) * cohabitating_dependant
         )
 
         return min_(caregiver_amount, p.max_amount)
