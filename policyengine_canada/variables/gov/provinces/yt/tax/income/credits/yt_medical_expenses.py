756a07162de6635218a17f3e71beaf80c3c31f6e
Zhirui Li <justinli@Zhiruis-MacBook-Pro-8.local>
2024-07-11 01:36:43 -0400

    new changes

--- policyengine_canada/variables/gov/provinces/yt/tax/income/credits/yt_medical_expenses.py
+++ policyengine_canada/variables/gov/provinces/yt/tax/income/credits/yt_medical_expenses.py
@@ -7,12 +7,10 @@ class yt_medical_expense_credit(Variable):
     label = "Yukon medical expenses"
     definition_period = YEAR
     defined_for = ProvinceCode.YT
-    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/5011-c/5011-c-23e.pdf"
+    reference = "https://www.canada.ca/content/dam/cra-arc/formspubs/pbg/5011-c/5011-c-23e.pdf" # page 23
 
     def formula(person, period, parameters):
         income = person("individual_net_income", period)
         p = parameters(period).gov.provinces.yt.medical
-        rate = p.rate
         income_fraction = income * p.rate
-        reduction_cap = p.reduction_cap
         return min_(income_fraction, p.reduction_cap)
