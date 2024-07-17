f9bc39454936b9e541a941dc8e727cb72eabadfe
karenlilr <167912915+karenlilr@users.noreply.github.com>
2024-07-17 07:21:00 +0000

    parameters, hard code Fixes #352

---
+++ policyengine_canada/variables/gov/provinces/nb/tax/income/nb_spouse_and_common_law_partner_amount_credit.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+class nb_spouse_and_common_law_partner_amount_credit(Variable):
+    value_type = float
+    entity = Household
+    label = "New Brunswick spouse and common-law partner amount credit"
+    definition_period = YEAR
+    defined_for = ProvinceCode.NB
+     
+
+    def formula(household, period, parameters):
+        p = parameters(period).gov.provinces.nb.tax.income.credits.spouse_or_common_law_partner_amount 
+        base_amount = p.base_amount  
+        max_credit = p.max_credit 
+        
+        spouse_income = add(household, period, ["spouse_income"])
+        reduced_amount = max_(base_amount - spouse_income, 0)  
+
